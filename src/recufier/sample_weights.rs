11809a7bccb1c0264b3783ffc6ee7a371312e6aa
ulrik elmelund <ulrikelmelund@gmail.com>
2022-11-22 21:56:35 +0100

    put the code into the file

--- src/recufier/sample_weights.rs
+++ src/recufier/sample_weights.rs
@@ -1,10 +1,61 @@
 #[allow(dead_code)]
 
 pub const SAMPLE_WEIGHTS: &str = "
-    sample_weights:
-        return
+sample_weights:
+    swap5
+    dup0
+    skiz
+        call sample_weights_body
+    pop pop pop pop pop pop
+    return
+
+sample_weights_body:
+    call decr
+    swap5
+    call dep5x6
+    call sample_weight
+    write_io write_io write_io
+    call sample_weights
+    return
+
+sample_weight:
+    push 0
+    push 0
+    push 0
+    push 0
+    hash
+    pop pop pop pop pop pop pop
+    return
+
+dup_digest:
+    dup4
+    dup4
+    dup4
+    dup4
+    dup4
+    return
+
+dep5x6:
+    dup5 dup5 dup5 dup5 dup5 dup5
+    return
+
+decr:
+    push -1
+    add
+    return
 ";
 
+pub const SAMPLE_WEIGHT: &str = "
+sample_weight:
+    push 0
+    push 0
+    push 0
+    push 0
+    hash
+    pop pop pop pop pop pop pop
+    write_io write_io write_io
+    return
+";
 #[cfg(test)]
 mod sample_weights_tests {
     use itertools::Itertools;
@@ -38,21 +89,75 @@ mod sample_weights_tests {
 
         XFieldElement::new([w0, w1, w2])
     }
+    // rust_output = [🥰😉🐶], [🍆🐍🐭], [😎😍👎]
+    // tasm_output = [🐷😎🐶], [😱🐱👏], [🤐😅😱]
+
+    // rust_output = [🤖🐭😅], [😆🐱🤑], [👍🤣🎂]
+    // tasm_output = [🐷😎🐶], [😱🐱👏], [🤐😅😱]
+
+    #[test]
+    fn run_sample_weight_equivalency_test() {
+        type H = RescuePrimeRegular;
+        let mut rng = rand::thread_rng();
+        let num_weights = 1;
+        let seed: Digest = H::hash(&BFieldElement::from(1u64));
+
+        let rust_output: Vec<BFieldElement> =
+            sample_weight::<H>(&seed, num_weights).coefficients.into();
+
+        let mut stdin = vec![BFieldElement::new(num_weights as u64)];
+
+        stdin.append(&mut seed.to_sequence());
+
+        let test_sample_weights: &str = "
+            main:
+                read_io
+                read_io
+                read_io
+                read_io
+                read_io
+                read_io
+                call sample_weight
+                write_io
+                write_io
+                write_io
+                halt
+        ";
+
+        let test_code = format!("{test_sample_weights}{SAMPLE_WEIGHT}");
+
+        let program = Program::from_code(&test_code).unwrap();
+
+        let (_states, tasm_output, _err) = program.run(stdin, vec![]);
+
+        for state in _states.iter() {
+            println!("{}", state);
+        }
+
+        let a = rust_output.iter().map(|bfe| bfe.emojihash()).join(", ");
+        let b = tasm_output.iter().map(|bfe| bfe.emojihash()).join(", ");
+        assert_eq!(
+            rust_output, tasm_output,
+            "\nrust_output = {}\ntasm_output = {}",
+            a, b,
+        );
+    }
 
     #[test]
     fn run_sample_weights_equivalency_test() {
         type H = RescuePrimeRegular;
         let mut rng = rand::thread_rng();
-        let num_weights = 4;
-        let seed: Digest = rng.gen();
+        let num_weights = 1;
+        let seed: Digest = H::hash(&BFieldElement::from(1u64));
 
         let rust_output: Vec<BFieldElement> = sample_weights::<H>(seed, num_weights)
             .iter()
             .flat_map(|xfe| xfe.coefficients)
             .collect();
 
-        let mut stdin = seed.to_sequence();
-        stdin.push(BFieldElement::new(num_weights as u64));
+        let mut stdin = vec![BFieldElement::new(num_weights as u64)];
+
+        stdin.append(&mut seed.to_sequence());
 
         let test_sample_weights: &str = "
             main:
@@ -83,10 +188,5 @@ mod sample_weights_tests {
             "\nrust_output = {}\ntasm_output = {}",
             a, b,
         );
-
-        // tasm:
-        // 4. kør programmet halt, navngiv det sample-weights: &str
-        // 5. assert at outputtet er identisk til rust-eqv. - dette skal selvfølgelig fejle til at starte med
-        // 6. pop string sample-weights??
     }
 }
