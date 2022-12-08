be284cd090fe664788ccdf1f600f6503d5e52425
Thorkil Vaerge <thor@neptune.cash>
2022-12-08 15:47:56 +0100

    Fix U32<2>'s powers of two function to leave jump stack unchanged
    This implementation is quite expensive in terms of computation cycles :/
    Over 1000 in the worst case.

--- src/arithmetic/u32s_2/powers_of_two.rs
+++ src/arithmetic/u32s_2/powers_of_two.rs
@@ -11,7 +11,7 @@ const SNIPPET_NAME: &str = "u32_2_powers_of_two";
 /// Consumes top element which is interpreted as exponent. Pushes a
 /// U32<2> to the top of the stack. So grows the stack by 1.
 pub struct U322PowersOfTwoMemory();
-pub struct U322PowersOfTwoLookup();
+pub struct U322PowersOfTwoArithmeticFlat();
 
 impl Snippet for U322PowersOfTwoMemory {
     const STACK_DIFF: isize = 1;
@@ -121,13 +121,74 @@ fn init_mem_code() -> String {
         .concat()
 }
 
-impl Snippet for U322PowersOfTwoLookup {
-    const STACK_DIFF: isize = -1;
+const ARITHMETIC_SNIPPET_NAME: &str = "u32_2_powers_of_two_lookup";
 
-    const NAME: &'static str = "u32_2_powers_of_two_lookup";
+impl Snippet for U322PowersOfTwoArithmeticFlat {
+    const STACK_DIFF: isize = 1;
+
+    const NAME: &'static str = ARITHMETIC_SNIPPET_NAME;
 
     fn get_function() -> String {
-        todo!()
+        let two_pow_32: &str = "4294967296";
+        format!(
+            "
+            // This carry function will at most be called once
+            {ARITHMETIC_SNIPPET_NAME}_carry:
+                pop
+                pop
+                push 1
+                push 1
+                return
+
+            // At init of this loop, the stack is:
+            // `exponent`
+            // `ires = result % 2^32`
+            // `indicator` which shows if `result = 2^32 * ires` or `result = ires`.
+            {ARITHMETIC_SNIPPET_NAME}_while:
+                push -1
+                add
+                swap2
+                swap1
+                push 2
+                mul
+                dup0
+                push {two_pow_32}
+                eq
+                skiz
+                    call {ARITHMETIC_SNIPPET_NAME}_carry
+                swap1
+                swap2
+
+                // If e == 0 return, otherwise recurse
+                dup0
+                push 0
+                eq
+                skiz
+                    return
+                recurse
+
+            {ARITHMETIC_SNIPPET_NAME}:
+                push 1
+                push 0
+                swap2
+
+                // e == 0 => ret = 1
+                dup0
+                skiz
+                    call {ARITHMETIC_SNIPPET_NAME}_while
+
+                // End of function: Check if indicator is set indicating
+                // that return value is greater than 2^32.
+                // Top of stack will always be zero here, so we can use
+                // that knowledge to construct the returned `U32s<2>`.
+                swap2
+                push 1
+                eq
+                skiz
+                    swap1
+                return
+                "
+        )
     }
 
     fn rust_shadowing(
@@ -147,7 +208,7 @@ mod tests {
 
     use super::*;
 
-    fn prop_exp(exponent: u8) {
+    fn prop_exp_memory(exponent: u8) {
         let mut init_stack = get_init_tvm_stack();
         init_stack.push(BFieldElement::new(exponent as u64));
 
@@ -185,15 +246,59 @@ mod tests {
     }
 
     #[test]
-    fn two_pow_zero_equals_one() {
-        prop_exp(0);
+    fn memory_two_pow_zero_equals_one() {
+        prop_exp_memory(0);
     }
 
     #[test]
-    fn all_exponents() {
-        prop_exp(2);
-        // for i in 1..64 {
-        //     prop_exp(i);
-        // }
+    fn memory_all_exponents() {
+        for i in 1..64 {
+            prop_exp_memory(i);
+        }
+    }
+
+    fn prop_exp_arithmetic_flat(exponent: u8) {
+        let mut init_stack = get_init_tvm_stack();
+        init_stack.push(BFieldElement::new(exponent as u64));
+
+        let mut tasm_stack = init_stack.clone();
+        let execution_result =
+            U322PowersOfTwoArithmeticFlat::run_tasm(&mut tasm_stack, vec![], vec![]);
+        println!(
+            "Cycle count for {} ({})`: {}",
+            U322PowersOfTwoArithmeticFlat::NAME,
+            exponent,
+            execution_result.cycle_count
+        );
+        println!(
+            "Hash table height for `{}`: {}",
+            U322PowersOfTwoArithmeticFlat::NAME,
+            execution_result.hash_table_height
+        );
+
+        let mut rust_stack = init_stack;
+        U322PowersOfTwoArithmeticFlat::rust_shadowing(&mut rust_stack, vec![], vec![]);
+        assert_eq!(
+            tasm_stack, rust_stack,
+            "Rust code must match TVM for `U322PowersOfTwo`"
+        );
+        let a = tasm_stack.pop().unwrap().value();
+        assert!(a < u32::MAX as u64);
+        let b = tasm_stack.pop().unwrap().value();
+        assert!(b < u32::MAX as u64);
+        let actual_res = U32s::<2>::new([a as u32, b as u32]);
+        let mut expected_res = U32s::<2>::one();
+        for _ in 0..exponent {
+            expected_res.mul_two();
+        }
+
+        assert_eq!(expected_res, actual_res);
+    }
+
+    #[test]
+    fn lookup_all_exponents() {
+        for i in 0..64 {
+            prop_exp_arithmetic_flat(i);
+        }
     }
 }
