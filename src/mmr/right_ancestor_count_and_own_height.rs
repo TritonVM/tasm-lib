de9f93db44dea8f39dcddf562810b76246e5c7e1
Thorkil Vaerge <thor@neptune.cash>
2023-01-04 13:45:02 +0100

    Add `right_ancestor_count_and_height` function for simple node arithmetic
    This function should reduce some MMR manipulation from being ln^2(N) to
    ln(N).

--- src/mmr/right_ancestor_count_and_own_height.rs
+++ src/mmr/right_ancestor_count_and_own_height.rs
@@ -1,6 +1,4 @@
 use std::collections::HashMap;
-
-use num::{One, Zero};
 use twenty_first::{shared_math::b_field_element::BFieldElement, util_types::mmr};
 
 use crate::{
@@ -12,9 +10,9 @@ use super::{
     left_child::MmrLeftChild, leftmost_ancestor::MmrLeftmostAncestor, right_child::MmrRightChild,
 };
 
-pub struct MmrRightChildAndHeight;
+pub struct MmrRightAncestorCountAndHeight;
 
-impl Snippet for MmrRightChildAndHeight {
+impl Snippet for MmrRightAncestorCountAndHeight {
     fn stack_diff() -> isize
     where
         Self: Sized,
@@ -122,7 +120,7 @@ impl Snippet for MmrRightChildAndHeight {
                 // Stack: _ ni_hi ni_lo rac height c_hi c_lo ni_hi ni_lo lc_hi lc_lo 1
                 pop
                 swap7
-                push1
+                push 1
                 add
                 swap7
                 // Stack: _ ni_hi ni_lo (rac + 1) height c_hi c_lo ni_hi ni_lo lc_hi lc_lo
@@ -170,16 +168,41 @@ impl Snippet for MmrRightChildAndHeight {
     ) where
         Self: Sized,
     {
+        // TODO: Remove this when twenty-first is updated
+        /// Traversing from this node upwards, count how many of the ancestor (including itself)
+        /// is a right child. This number is used to determine how many nodes to insert when a
+        /// new leaf is added.
+        fn right_ancestor_count_and_own_height(node_index: u128) -> (u32, u32) {
+            let (mut candidate, mut candidate_height) = mmr::shared::leftmost_ancestor(node_index);
+
+            // leftmost ancestor is always a left node, so count starts at 0.
+            let mut right_ancestor_count = 0;
+
+            loop {
+                if candidate == node_index {
+                    return (right_ancestor_count, candidate_height as u32);
+                }
+
+                let left_child = mmr::shared::left_child(candidate, candidate_height);
+                let candidate_is_right_child = left_child < node_index;
+                if candidate_is_right_child {
+                    candidate = mmr::shared::right_child(candidate);
+                    right_ancestor_count += 1;
+                } else {
+                    candidate = left_child;
+                    right_ancestor_count = 0;
+                };
+
+                candidate_height -= 1;
+            }
+        }
+
         let node_index_lo: u32 = stack.pop().unwrap().try_into().unwrap();
         let node_index_hi: u32 = stack.pop().unwrap().try_into().unwrap();
         let node_index: u64 = (node_index_hi as u64) * (1u64 << 32) + node_index_lo as u64;
 
-        let (ret, height) = mmr::shared::right_child_and_height(node_index as u128);
-        stack.push(if ret {
-            BFieldElement::one()
-        } else {
-            BFieldElement::zero()
-        });
+        let (ret, height) = right_ancestor_count_and_own_height(node_index as u128);
+        stack.push(BFieldElement::new(ret as u64));
 
         stack.push(BFieldElement::new(height as u64));
     }
@@ -187,12 +210,92 @@ impl Snippet for MmrRightChildAndHeight {
 
 #[cfg(test)]
 mod tests {
-    use twenty_first::{
-        amount::u32s::U32s, shared_math::b_field_element::BFieldElement,
-        util_types::algebraic_hasher::Hashable,
-    };
+    use twenty_first::shared_math::b_field_element::BFieldElement;
 
     use crate::{get_init_tvm_stack, snippet_trait::rust_tasm_equivalence_prop};
 
     use super::*;
+
+    #[test]
+    fn right_ancestor_count_test() {
+        prop_right_ancestor_count_and_own_height(1, 0, 0);
+        prop_right_ancestor_count_and_own_height(2, 1, 0);
+        prop_right_ancestor_count_and_own_height(3, 0, 1);
+        prop_right_ancestor_count_and_own_height(4, 0, 0);
+        prop_right_ancestor_count_and_own_height(5, 2, 0);
+        prop_right_ancestor_count_and_own_height(6, 1, 1);
+        prop_right_ancestor_count_and_own_height(7, 0, 2);
+        prop_right_ancestor_count_and_own_height(8, 0, 0);
+        prop_right_ancestor_count_and_own_height(9, 1, 0);
+
+        prop_right_ancestor_count_and_own_height(10, 0, 1);
+        prop_right_ancestor_count_and_own_height(11, 0, 0);
+        prop_right_ancestor_count_and_own_height(12, 3, 0);
+        prop_right_ancestor_count_and_own_height(13, 2, 1);
+        prop_right_ancestor_count_and_own_height(14, 1, 2);
+        prop_right_ancestor_count_and_own_height(15, 0, 3);
+        prop_right_ancestor_count_and_own_height(16, 0, 0);
+        prop_right_ancestor_count_and_own_height(17, 1, 0);
+        prop_right_ancestor_count_and_own_height(18, 0, 1);
+        prop_right_ancestor_count_and_own_height(19, 0, 0);
+
+        prop_right_ancestor_count_and_own_height(20, 2, 0);
+        prop_right_ancestor_count_and_own_height(21, 1, 1);
+        prop_right_ancestor_count_and_own_height(22, 0, 2);
+        prop_right_ancestor_count_and_own_height(23, 0, 0);
+        prop_right_ancestor_count_and_own_height(24, 1, 0);
+        prop_right_ancestor_count_and_own_height(25, 0, 1);
+        prop_right_ancestor_count_and_own_height(26, 0, 0);
+        prop_right_ancestor_count_and_own_height(27, 4, 0);
+        prop_right_ancestor_count_and_own_height(28, 3, 1);
+        prop_right_ancestor_count_and_own_height(29, 2, 2);
+
+        prop_right_ancestor_count_and_own_height(30, 1, 3);
+        prop_right_ancestor_count_and_own_height(31, 0, 4);
+        prop_right_ancestor_count_and_own_height(32, 0, 0);
+        prop_right_ancestor_count_and_own_height(33, 1, 0);
+        prop_right_ancestor_count_and_own_height(34, 0, 1);
+        prop_right_ancestor_count_and_own_height(35, 0, 0);
+        prop_right_ancestor_count_and_own_height(36, 2, 0);
+        prop_right_ancestor_count_and_own_height(37, 1, 1);
+        prop_right_ancestor_count_and_own_height(38, 0, 2);
+        prop_right_ancestor_count_and_own_height(39, 0, 0);
+
+        prop_right_ancestor_count_and_own_height(40, 1, 0);
+        prop_right_ancestor_count_and_own_height(41, 0, 1);
+
+        // Run test for big numbers
+        for i in 0..63 {
+            prop_right_ancestor_count_and_own_height((u64::MAX >> 1) - i, i as u32, 62 - i as u32);
+        }
+    }
+
+    fn prop_right_ancestor_count_and_own_height(
+        node_index: u64,
+        expected_count: u32,
+        expected_height: u32,
+    ) {
+        let mut init_stack = get_init_tvm_stack();
+        let node_index_hi = BFieldElement::new(node_index >> 32);
+        let node_index_lo = BFieldElement::new(node_index & u32::MAX as u64);
+        init_stack.push(node_index_hi);
+        init_stack.push(node_index_lo);
+
+        // _ (right_lineage_count:u32) (height: u32)
+        let expected = vec![
+            get_init_tvm_stack(),
+            vec![
+                BFieldElement::new(expected_count as u64),
+                BFieldElement::new(expected_height as u64),
+            ],
+        ]
+        .concat();
+        let _execution_result = rust_tasm_equivalence_prop::<MmrRightAncestorCountAndHeight>(
+            &init_stack,
+            &[],
+            &[],
+            &mut HashMap::default(),
+            Some(&expected),
+        );
+    }
 }
