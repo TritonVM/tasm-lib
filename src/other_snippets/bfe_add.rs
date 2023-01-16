457467918dd88c0ae202c258f6d30009b77d3ba7
Simon Shine <simon@simonshine.dk>
2023-01-16 18:28:08 +0100

    impl NewSnippet for BfeAdd

--- src/other_snippets/bfe_add.rs
+++ src/other_snippets/bfe_add.rs
@@ -1,11 +1,35 @@
 use std::collections::HashMap;
 
+use rand::Rng;
 use twenty_first::shared_math::b_field_element::BFieldElement;
 
 use crate::library::Library;
-use crate::snippet::Snippet;
+use crate::snippet::{NewSnippet, Snippet};
+use crate::{get_init_tvm_stack, ExecutionState};
 
 pub struct BfeAdd();
+
+impl NewSnippet for BfeAdd {
+    fn inputs() -> Vec<&'static str> {
+        vec!["b", "a"]
+    }
+
+    fn outputs() -> Vec<&'static str> {
+        vec!["a + b"]
+    }
+
+    fn crash_conditions() -> Vec<&'static str> {
+        vec![]
+    }
+
+    fn gen_input_states() -> Vec<ExecutionState> {
+        let mut rng = rand::thread_rng();
+        let mut stack = get_init_tvm_stack();
+        stack.push(rng.gen());
+        stack.push(rng.gen());
+        vec![ExecutionState::with_stack(stack)]
+    }
+}
 
 impl Snippet for BfeAdd {
     fn stack_diff() -> isize {
@@ -41,28 +65,18 @@ impl Snippet for BfeAdd {
 
 #[cfg(test)]
 mod tests {
-    use crate::get_init_tvm_stack;
-    use crate::test_helpers::rust_tasm_equivalence_prop;
+    use crate::snippet_bencher::bench_and_write;
+    use crate::test_helpers::rust_tasm_equivalence_prop_new;
 
     use super::*;
 
     #[test]
-    fn bfe_add_simple() {
-        // TASM
-        let init_stack = vec![
-            get_init_tvm_stack(),
-            vec![BFieldElement::new(200), BFieldElement::new(300)],
-        ]
-        .concat();
+    fn swap_digest_test() {
+        rust_tasm_equivalence_prop_new::<BfeAdd>();
+    }
 
-        let expected = None;
-        let _execution_result = rust_tasm_equivalence_prop::<BfeAdd>(
-            &init_stack,
-            &[],
-            &[],
-            &mut HashMap::default(),
-            0,
-            expected,
-        );
+    #[test]
+    fn swap_digest_benchmark() {
+        bench_and_write::<BfeAdd>();
     }
 }
