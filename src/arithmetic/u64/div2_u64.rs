c749ab6a93fab9ef443488bce3c1165ca4032c5f
sword-smith <thv@neptune.cash>
2023-01-25 01:59:07 +0100

    Implement NewSnippet for div2_u64

--- src/arithmetic/u64/div2_u64.rs
+++ src/arithmetic/u64/div2_u64.rs
@@ -1,11 +1,38 @@
 use std::collections::HashMap;
 
+use rand::RngCore;
+use twenty_first::amount::u32s::U32s;
 use twenty_first::shared_math::b_field_element::BFieldElement;
 
 use crate::library::Library;
-use crate::snippet::Snippet;
+use crate::snippet::{NewSnippet, Snippet};
+use crate::{get_init_tvm_stack, push_hashable, ExecutionState};
 
 pub struct Div2U64;
+
+impl NewSnippet for Div2U64 {
+    fn inputs() -> Vec<&'static str> {
+        vec!["value_hi", "value_lo"]
+    }
+
+    fn outputs() -> Vec<&'static str> {
+        vec!["(value / 2)_hi", "(value / 2)_lo"]
+    }
+
+    fn crash_conditions() -> Vec<&'static str> {
+        vec!["If value_hi is not a u32", "If value_lo is not a u32"]
+    }
+
+    fn gen_input_states() -> Vec<crate::ExecutionState> {
+        let n: u64 = rand::thread_rng().next_u64();
+        let n: U32s<2> = n.try_into().unwrap();
+        let mut input_stack = get_init_tvm_stack();
+
+        push_hashable(&mut input_stack, &n);
+
+        vec![ExecutionState::with_stack(input_stack)]
+    }
+}
 
 impl Snippet for Div2U64 {
     fn stack_diff() -> isize {
@@ -76,9 +103,48 @@ mod tests {
     use twenty_first::shared_math::b_field_element::BFieldElement;
 
     use crate::get_init_tvm_stack;
-    use crate::test_helpers::rust_tasm_equivalence_prop;
+    use crate::test_helpers::{rust_tasm_equivalence_prop, rust_tasm_equivalence_prop_new};
 
     use super::*;
+
+    #[test]
+    fn div_2_test_new_snippet() {
+        rust_tasm_equivalence_prop_new::<Div2U64>();
+    }
+
+    #[should_panic]
+    #[test]
+    fn lo_is_not_u32() {
+        let mut init_stack = get_init_tvm_stack();
+        init_stack.push(BFieldElement::new(16));
+        init_stack.push(BFieldElement::new(u32::MAX as u64 + 1));
+
+        let _execution_result = rust_tasm_equivalence_prop::<Div2U64>(
+            &init_stack,
+            &[],
+            &[],
+            &mut HashMap::default(),
+            0,
+            None,
+        );
+    }
+
+    #[should_panic]
+    #[test]
+    fn hi_is_not_u32() {
+        let mut init_stack = get_init_tvm_stack();
+        init_stack.push(BFieldElement::new(u32::MAX as u64 + 1));
+        init_stack.push(BFieldElement::new(16));
+
+        let _execution_result = rust_tasm_equivalence_prop::<Div2U64>(
+            &init_stack,
+            &[],
+            &[],
+            &mut HashMap::default(),
+            0,
+            None,
+        );
+    }
 
     #[test]
     fn div_2_test() {
