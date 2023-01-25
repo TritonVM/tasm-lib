5f509549d7c849f06787a397044c5fef1a1fa9b6
sword-smith <thv@neptune.cash>
2023-01-25 01:18:44 +0100

    Remove two bad implementations of pow2_u64

--- src/arithmetic/u64/pow2_u64.rs
+++ src/arithmetic/u64/pow2_u64.rs
@@ -11,8 +11,6 @@ use crate::{get_init_tvm_stack, push_hashable, ExecutionState};
 
 /// Consumes top element which is interpreted as exponent. Pushes a
 /// U32<2> to the top of the stack. So grows the stack by 1.
-pub struct Pow2MemoryUnsafeU64();
-pub struct Pow2ArithmeticFlatU64();
 pub struct Pow2StaticU64();
 
 impl NewSnippet for Pow2StaticU64 {
@@ -111,187 +109,6 @@ impl Snippet for Pow2StaticU64 {
 
     fn rust_shadowing(
         stack: &mut Vec<BFieldElement>,
-        std_in: Vec<BFieldElement>,
-        secret_in: Vec<BFieldElement>,
-        memory: &mut HashMap<BFieldElement, BFieldElement>,
-    ) {
-        Pow2ArithmeticFlatU64::rust_shadowing(stack, std_in, secret_in, memory)
-    }
-}
-
-impl Snippet for Pow2MemoryUnsafeU64 {
-    fn stack_diff() -> isize {
-        1
-    }
-
-    fn entrypoint() -> &'static str {
-        "pow2_u64"
-    }
-
-    // Assumes that the top stack element is below 64. Otherwise undefined.
-    fn function_body(library: &mut Library) -> String {
-        let entrypoint = Self::entrypoint();
-        let lookup_addr = library.kmalloc(32);
-
-        // FIXME: Initializing the lookup table only needs to happen once.
-        let init_mem_code: String = (0..32)
-            .map(|i| {
-                let addr_i = lookup_addr + i;
-                let two_pow_i = 2u32.pow(i as u32);
-                format!("push {addr_i} push {two_pow_i} write_mem pop pop\n")
-            })
-            .collect::<Vec<_>>()
-            .concat();
-
-        format!(
-            "
-            // Before: _ i      where i in [0;64[
-            // After:  _ hi lo  where (hi,lo) is a U32s<2> of 2^i
-            {entrypoint}:
-                call {entrypoint}_init_mem
-                push 0       // -> _ i 0
-                read_mem     // -> _ i (2^i)|0
-
-                push 1 dup1  // -> _ i (2^i)|0 1 (2^i)|0
-                  skiz call {entrypoint}_lo_then   // skiz = don't skip if positive
-                  skiz call {entrypoint}_hi_else   // -> _ hi lo, we popped 1 or 0
-
-                return
-
-            // Before: _ i (2^i) 1
-            // After:  _ 0 (2^i) 0
-            {entrypoint}_lo_then:
-                push 0   // -> _ i (2^i) 1 0
-                swap3    // -> _ 0 (2^i) 1 i
-                pop pop  // -> _ 0 (2^i)
-                push 0   // -> _ 0 (2^i) 0
-                return
-
-            // Before: _ i (2^i)
-            // After:  _ (2^(i - 32)) 0 0
-            {entrypoint}_hi_else:
-                pop        // -> _ i
-                push -32
-                add        // -> _ (i-32)
-                push 0     // -> _ (i-32) 0
-                read_mem   // -> _ (i-32) (2^(i-32))
-                swap1
-                pop        // -> _ (2^(i-32))
-                push 0     // -> _ (2^(i-32)) 0
-                return
-
-            {entrypoint}_init_mem:
-                {init_mem_code}
-                return
-            "
-        )
-    }
-
-    fn rust_shadowing(
-        stack: &mut Vec<BFieldElement>,
-        _std_in: Vec<BFieldElement>,
-        _secret_in: Vec<BFieldElement>,
-        memory: &mut HashMap<BFieldElement, BFieldElement>,
-    ) {
-        // Insert all powers of two into memory, since that's what the TASM code does
-        for i in 0..32 {
-            memory.insert(
-                BFieldElement::new(i),
-                BFieldElement::new(2u32.pow(i as u32) as u64),
-            );
-        }
-
-        // Find exponent
-        let mut exponent: u32 = stack.pop().unwrap().try_into().unwrap();
-        let mut res = U32s::<2>::one();
-
-        while exponent > 0 {
-            res.mul_two();
-            exponent -= 1;
-        }
-
-        let mut res = res.to_sequence();
-        for _ in 0..res.len() {
-            stack.push(res.pop().unwrap());
-        }
-    }
-}
-
-impl Snippet for Pow2ArithmeticFlatU64 {
-    fn stack_diff() -> isize {
-        1
-    }
-
-    fn entrypoint() -> &'static str {
-        "pow2_u64"
-    }
-
-    fn function_body(_library: &mut Library) -> String {
-        let entrypoint = Self::entrypoint();
-        let two_pow_32: &str = "4294967296";
-        format!(
-            "
-            // This carry function will at most be called once
-            {entrypoint}_carry:
-                pop
-                pop
-                push 1
-                push 1
-                return
-
-            // At init of this loop, the stack is:
-            // `exponent`
-            // `ires = result % 2^32`
-            // `indicator` which shows if `result = 2^32 * ires` or `result = ires`.
-            {entrypoint}_while:
-                push -1
-                add
-                swap2
-                swap1
-                push 2
-                mul
-                dup0
-                push {two_pow_32}
-                eq
-                skiz
-                    call {entrypoint}_carry
-                swap1
-                swap2
-
-                // If e == 0 return, otherwise recurse
-                dup0
-                push 0
-                eq
-                skiz
-                    return
-                recurse
-
-            {entrypoint}:
-                push 1
-                push 0
-                swap2
-
-                // e == 0 => ret = 1
-                dup0
-                skiz
-                    call {entrypoint}_while
-
-                // End of function: Check if indicator is set indicating
-                // that return value is greater than 2^32.
-                // Top of stack will always be zero here, so we can use
-                // that knowledge to construct the returned `U32s<2>`.
-                swap2
-                push 1
-                eq
-                skiz
-                    swap1
-                return
-            "
-        )
-    }
-
-    fn rust_shadowing(
-        stack: &mut Vec<BFieldElement>,
         _std_in: Vec<BFieldElement>,
         _secret_in: Vec<BFieldElement>,
         _memory: &mut HashMap<BFieldElement, BFieldElement>,
@@ -332,68 +149,6 @@ mod tests {
         bench_and_write::<Pow2StaticU64>();
     }
 
-    fn prop_exp_memory(exponent: u8) {
-        let mut init_stack = get_init_tvm_stack();
-        init_stack.push(BFieldElement::new(exponent as u64));
-
-        let expected = None;
-        let mut execution_result = rust_tasm_equivalence_prop::<Pow2MemoryUnsafeU64>(
-            &init_stack,
-            &[],
-            &[],
-            &mut HashMap::default(),
-            0,
-            expected,
-        );
-
-        let a = execution_result.final_stack.pop().unwrap().value();
-        assert!(a < u32::MAX as u64);
-
-        let b = execution_result.final_stack.pop().unwrap().value();
-        assert!(b < u32::MAX as u64);
-
-        let actual_res = U32s::<2>::new([a as u32, b as u32]);
-        let mut expected_res = U32s::<2>::one();
-        for _ in 0..exponent {
-            expected_res.mul_two();
-        }
-        assert_eq!(expected_res, actual_res);
-    }
-
-    #[test]
-    fn all_exponents_memory() {
-        for i in 0..64 {
-            prop_exp_memory(i);
-        }
-    }
-
-    fn prop_exp_arithmetic_flat(exponent: u8) {
-        let mut init_stack = get_init_tvm_stack();
-        init_stack.push(BFieldElement::new(exponent as u64));
-
-        let expected = None;
-        let mut execution_result = rust_tasm_equivalence_prop::<Pow2ArithmeticFlatU64>(
-            &init_stack,
-            &[],
-            &[],
-            &mut HashMap::default(),
-            0,
-            expected,
-        );
-
-        let a = execution_result.final_stack.pop().unwrap().value();
-        assert!(a < u32::MAX as u64);
-        let b = execution_result.final_stack.pop().unwrap().value();
-        assert!(b < u32::MAX as u64);
-        let actual_res = U32s::<2>::new([a as u32, b as u32]);
-        let mut expected_res = U32s::<2>::one();
-        for _ in 0..exponent {
-            expected_res.mul_two();
-        }
-
-        assert_eq!(expected_res, actual_res);
-    }
-
     fn prop_exp_static(exponent: u8) {
         let mut init_stack = get_init_tvm_stack();
         init_stack.push(BFieldElement::new(exponent as u64));
@@ -419,13 +174,6 @@ mod tests {
         }
 
         assert_eq!(expected_res, actual_res);
-    }
-
-    #[test]
-    fn all_exponents_arithmetic_flat() {
-        for i in 0..64 {
-            prop_exp_arithmetic_flat(i);
-        }
     }
 
     #[test]
