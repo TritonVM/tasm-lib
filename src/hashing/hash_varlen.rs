8ce999dda67d327c0dbab71b52cdacced4fbd2c0
Thorkil Vaerge <thor@neptune.cash>
2023-06-02 15:14:43 +0200

    WIP: hash_varlen works for empty input

--- src/hashing/hash_varlen.rs
+++ src/hashing/hash_varlen.rs
@@ -38,7 +38,13 @@ impl Snippet for HashVarlen {
     where
         Self: Sized,
     {
-        vec!["Hasher::hash_varlen(&[*addr..*addr + length])".to_string()]
+        vec![
+            "elemement_4".to_string(),
+            "elemement_3".to_string(),
+            "elemement_2".to_string(),
+            "elemement_1".to_string(),
+            "elemement_0".to_string(),
+        ]
     }
 
     fn stack_diff(&self) -> isize
@@ -60,8 +66,11 @@ impl Snippet for HashVarlen {
             // absorb all chunks of 10 elements
             push 1
             call {entrypoint}_loop
-            // _ *addr length first_time
-            pop // _ *addr length
+            // _ *addr first_time length
+
+            swap 1
+            pop
+            // _ *addr length
 
             // read remaining elements from memory
             // for now we assume that length = 0 (mod 10)
@@ -85,101 +94,91 @@ impl Snippet for HashVarlen {
                 push 10 // _ *addr first_time length 10
                 dup 1 // _ *addr first_time length 10 length
                 lt // _ *addr first_time length (length < 10)
-                swap 1 // _ *addr first_time (length < 10) length
-                swap 2 // _ *addr length (length < 10) first_time
-                swap 1 // _ *addr length first_time (length < 10)
+
                 // check condition
                 skiz
                     return
-                // _ *addr length first_time
+                // _ *addr first_time length
 
                 // body
                 // read 10 elements to stack
-                swap 1 // _ length first_time *addr
-                dup 0 // _ length first_time *addr *addr
-                push 9 // _ length first_time *addr *addr 9
-                add // _ length first_time  *addr (*addr + 9)
+                swap 2   // _ length first_time *addr
+                dup 0    // _ length first_time *addr *addr
+                push 9   // _ length first_time *addr *addr 9
+                add      // _ length first_time *addr (*addr+9)
                 read_mem // _ length first_time *addr (*addr+9) element_9
-                swap 1 // _ length first_time *addr element_9 (*addr+9)
-                push -1 // _ length first_time *addr element_9 (*addr+9) -1
-                add // _ length first_time *addr element_9 (*addr+8)
+                swap 1   // _ length first_time *addr element_9 (*addr+9)
+                push -1  // _ length first_time *addr element_9 (*addr+9) -1
+                add      // _ length first_time *addr element_9 (*addr+8)
                 read_mem // _ length first_time *addr element_9 (*add+8) element_8
-                swap 1 // _ length first_time *addr element_9 element_8 (*addr+8)
-                push -1 // _ length first_time *addr element_9 element_8 (*addr+8) -1
-                add // _ length first_time *addr element_9 element_8 (*addr+7)
+                swap 1   // _ length first_time *addr element_9 element_8 (*addr+8)
+                push -1  // _ length first_time *addr element_9 element_8 (*addr+8) -1
+                add      // _ length first_time *addr element_9 element_8 (*addr+7)
                 read_mem // _ length first_time *addr element_9 element_8 (*addr+7) element_7
-                swap 1 // _ length first_time *addr element_9 element_8 element_7 (*addr+7)
-                push -1 // _ length first_time *addr element_9 element_8 element_7 (*addr+7) -1
-                add // _ length first_time *addr element_9 element_8 element_7 (*addr+6)
+                swap 1   // _ length first_time *addr element_9 element_8 element_7 (*addr+7)
+                push -1  // _ length first_time *addr element_9 element_8 element_7 (*addr+7) -1
+                add      // _ length first_time *addr element_9 element_8 element_7 (*addr+6)
                 read_mem // _ length first_time *addr element_9 element_8 element_7 (*addr+6) element_6
-                swap 1 // _ length first_time *addr element_9 element_8 element_7 element_6 (*addr+6)
-                push -1 // _ length first_time *addr element_9 element_8 element_7 element_6 (*addr+6) -1
-                add // _ length first_time *addr element_9 element_8 element_7 element_6 (*addr+5)
+                swap 1   // _ length first_time *addr element_9 element_8 element_7 element_6 (*addr+6)
+                push -1  // _ length first_time *addr element_9 element_8 element_7 element_6 (*addr+6) -1
+                add      // _ length first_time *addr element_9 element_8 element_7 element_6 (*addr+5)
                 read_mem // _ length first_time *addr element_9 element_8 element_7 element_6 (*addr+5) element_5
-                swap 1 // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 (*addr+5)
-                push -1 // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 (*addr+5) -1
-                add // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 (*addr+4)
+                swap 1   // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 (*addr+5)
+                push -1  // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 (*addr+5) -1
+                add      // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 (*addr+4)
                 read_mem // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 (*addr+4) element_4
-                swap 1 // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 (*addr+4)
-                push -1 // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 (*addr+4) -1
-                add // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 (*addr+3)
+                swap 1   // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 (*addr+4)
+                push -1  // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 (*addr+4) -1
+                add      // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 (*addr+3)
                 read_mem // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 (*addr+3) element_3
-                swap 1 // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 (*addr+3)
-                push -1 // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 (*addr+3) -1
-                add // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 (*addr+2)
+                swap 1   // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 (*addr+3)
+                push -1  // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 (*addr+3) -1
+                add      // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 (*addr+2)
                 read_mem // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 (*addr+2) element_2
-                swap 1 // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 (*addr+2)
-                push -1 // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 (*addr+2) -1
-                add // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 (*addr+1)
+                swap 1   // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 (*addr+2)
+                push -1  // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 (*addr+2) -1
+                add      // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 (*addr+1)
                 read_mem // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 (*addr+1) element_1
-                swap 1 // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 (*addr+1)
-                push -1 // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 (*addr+1) -1
-                add // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 *addr
+                swap 1   // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 (*addr+1)
+                push -1  // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 (*addr+1) -1
+                add      // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 *addr
                 read_mem // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 *addr element_0
-                swap 1 // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0 *addr
-                pop // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0
+                swap 1   // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0 *addr
+                pop      // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0
 
-                // How to pick the right branch
-                // _ 1 condition
-                // skiz
-                //     call {entrypoint}_if_branch
-                // skiz
-                //     call {entrypoint}_else_branch
-                // {entrypoint}_if_branch:
-                // pop
-                // [body]
-                // push 0
-                // return
-
-                dup 0 // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0 element_0
-                swap 12 // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0 first_time
-                push 1 // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0 first_time 1
-                swap 1 // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0 1 first_time
+                dup 0    // _ length first_time *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0 element_0
+                swap 12  // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0 first_time
+                push 1   // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0 first_time 1
+                swap 1   // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0 1 first_time
                 skiz
                     call {entrypoint}_if_branch
                 skiz
                     absorb
-                // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0
-                pop pop pop pop pop pop pop pop pop pop // _ length element_0 *addr
-                swap 1 // _ length *addr element_0
-                pop // _ length *addr
 
-                push 10 // _ length *addr 10
-                add // _ length (*addr+10)
-                swap 1 // _ (*addr+10) length
+                // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0
+
+                pop pop pop pop pop pop pop pop pop pop
+                // _ length element_0 *addr
+
+                swap 1   // _ length *addr element_0
+                pop      // _ length *addr
+
+                push 10  // _ length *addr 10
+                add      // _ length (*addr+10)
+                swap 1   // _ (*addr+10) length
                 push -10 // _ (*addr+10) length -10
-                add // _ (*addr+10) (length-10)
-                push 0 // _ (*addr+10) (length-10) 0
+                add      // _ (*addr+10) (length-10)
+                push 0   // _ (*addr+10) (length-10) 0
 
                 recurse
 
                 // BEFORE: // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0 1
-                // AFTER: // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0 0
+                // AFTER:  // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0 0
                 {entrypoint}_if_branch:
-                pop // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0
-                absorb_init // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0
-                push 0
-                return
+                    pop         // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0
+                    absorb_init // _ length element_0 *addr element_9 element_8 element_7 element_6 element_5 element_4 element_3 element_2 element_1 element_0
+                    push 0
+                    return
                 "
         )
     }
@@ -196,7 +195,40 @@ impl Snippet for HashVarlen {
     where
         Self: Sized,
     {
-        let empty_memory_state = ExecutionState {
+        let empty_memory_state_read_0 = ExecutionState {
+            stack: vec![
+                get_init_tvm_stack(),
+                vec![BFieldElement::one(), BFieldElement::new(0)],
+            ]
+            .concat(),
+            std_in: vec![],
+            secret_in: vec![],
+            memory: HashMap::new(),
+            words_allocated: 1,
+        };
+        let empty_memory_state_read_10 = ExecutionState {
+            stack: vec![
+                get_init_tvm_stack(),
+                vec![BFieldElement::one(), BFieldElement::new(30)],
+            ]
+            .concat(),
+            std_in: vec![],
+            secret_in: vec![],
+            memory: HashMap::new(),
+            words_allocated: 1,
+        };
+        let empty_memory_state_read_20 = ExecutionState {
+            stack: vec![
+                get_init_tvm_stack(),
+                vec![BFieldElement::one(), BFieldElement::new(30)],
+            ]
+            .concat(),
+            std_in: vec![],
+            secret_in: vec![],
+            memory: HashMap::new(),
+            words_allocated: 1,
+        };
+        let empty_memory_state_read_30 = ExecutionState {
             stack: vec![
                 get_init_tvm_stack(),
                 vec![BFieldElement::one(), BFieldElement::new(30)],
@@ -208,7 +240,12 @@ impl Snippet for HashVarlen {
             words_allocated: 1,
         };
 
-        vec![empty_memory_state]
+        vec![
+            empty_memory_state_read_0,
+            empty_memory_state_read_10,
+            empty_memory_state_read_20,
+            empty_memory_state_read_30,
+        ]
     }
 
     fn common_case_input_state(&self) -> crate::ExecutionState
@@ -234,7 +271,6 @@ impl Snippet for HashVarlen {
     ) where
         Self: Sized,
     {
-        // todo!()
         let length: u32 = stack.pop().unwrap().try_into().unwrap();
         let memory_pointer: BFieldElement = stack.pop().unwrap();
 
