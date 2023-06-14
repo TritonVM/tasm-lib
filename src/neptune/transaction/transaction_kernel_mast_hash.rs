0d28979a781ac83d618aa91442fafead73c0f96f
sword-smith <thv@neptune.cash>
2023-06-14 18:20:20 +0200

    Align most comments in tx kernel mast hash

--- src/neptune/transaction/transaction_kernel_mast_hash.rs
+++ src/neptune/transaction/transaction_kernel_mast_hash.rs
@@ -117,156 +117,156 @@ impl Snippet for TransactionKernelMastHash {
         // AFTER: _ d4 d3 d2 d1 d0
         {entrypoint}:
             // allocate new list of 16 digests
-            push 16 // _ *kernel 16
-            dup 0 // _ *kernel 16 16
-            call {new_list} // _ *kernel 16 *list
-            swap 1 // _ *kernel *list 16
-            call {set_length} // _ *kernel *list
+            push 16                      // _ *kernel 16
+            dup 0                        // _ *kernel 16 16
+            call {new_list}              // _ *kernel 16 *list
+            swap 1                       // _ *kernel *list 16
+            call {set_length}            // _ *kernel *list
 
             // populate list[8] with inputs digest
-            dup 1 // _ *kernel *list *kernel
-            call {get_field_inputs} // _ *kernel *list *inputs
-            dup 0 // _ *kernel *list *inputs *inputs
+            dup 1                       // _ *kernel *list *kernel
+            call {get_field_inputs}     // _ *kernel *list *inputs
+            dup 0                       // _ *kernel *list *inputs *inputs
             call {get_field_size_inputs} // _ *kernel *list *inputs *inputs_size
-            call {hash_varlen} // _ *kernel *list d4 d3 d2 d1 d0
-            dup 5 push 8 // _ *kernel *list d4 d3 d2 d1 d0 *list 8
-            call {set_element} // _ *kernel *list
+            call {hash_varlen}          // _ *kernel *list d4 d3 d2 d1 d0
+            dup 5 push 8                // _ *kernel *list d4 d3 d2 d1 d0 *list 8
+            call {set_element}          // _ *kernel *list
 
             // populate list[9] with outputs digest
-            dup 1 // _ *kernel *list *kernel
-            call {get_field_outputs} // _ *kernel *list *outputs
-            dup 2 // _ *kernel *list *outputs *kernel
+            dup 1                       // _ *kernel *list *kernel
+            call {get_field_outputs}    // _ *kernel *list *outputs
+            dup 2                       // _ *kernel *list *outputs *kernel
             call {get_field_size_outputs} // _ *kernel *list *outputs *outputs_size
-            call {hash_varlen} // _ *kernel *list d4 d3 d2 d1 d0
-            dup 5 push 9 // _ *kernel *list d4 d3 d2 d1 d0 *list 9
-            call {set_element} // _ *kernel *list
+            call {hash_varlen}          // _ *kernel *list d4 d3 d2 d1 d0
+            dup 5 push 9                // _ *kernel *list d4 d3 d2 d1 d0 *list 9
+            call {set_element}          // _ *kernel *list
 
             // populate list[10] with pubscript_hashes_and_inputs digest
-            dup 1 // _ *kernel *list *kernel
+            dup 1                       // _ *kernel *list *kernel
             call {get_field_pubscript_hashes_and_inputs} // _ *kernel *list *pubscript_hashes_and_inputs
-            dup 2 // _ *kernel *list *pubscript_hashes_and_inputs *kernel
+            dup 2                       // _ *kernel *list *pubscript_hashes_and_inputs *kernel
             call {get_field_size_pubscript_hashes_and_inputs} // _ *kernel *list *pubscript_hashes_and_inputs *pubscript_hashes_and_inputs_size_size
-            call {hash_varlen} // _ *kernel *list d4 d3 d2 d1 d0
-            dup 5 push 10 // _ *kernel *list d4 d3 d2 d1 d0 *list 10
-            call {set_element} // _ *kernel *list
+            call {hash_varlen}          // _ *kernel *list d4 d3 d2 d1 d0
+            dup 5 push 10               // _ *kernel *list d4 d3 d2 d1 d0 *list 10
+            call {set_element}          // _ *kernel *list
 
             // populate list[11] with fee digest
-            dup 1 // _ *kernel *list *kernel
-            call {get_field_fee} // _ *kernel *list *fee
-            dup 2 // _ *kernel *list *fee *kernel
-            call {get_field_size_fee} // _ *kernel *list *fee *fee_size
-            call {hash_varlen} // _ *kernel *list d4 d3 d2 d1 d0
-            dup 5 push 11 // _ *kernel *list d4 d3 d2 d1 d0 *list 11
-            call {set_element} // _ *kernel *list
+            dup 1                       // _ *kernel *list *kernel
+            call {get_field_fee}        // _ *kernel *list *fee
+            dup 2                       // _ *kernel *list *fee *kernel
+            call {get_field_size_fee}   // _ *kernel *list *fee *fee_size
+            call {hash_varlen}          // _ *kernel *list d4 d3 d2 d1 d0
+            dup 5 push 11               // _ *kernel *list d4 d3 d2 d1 d0 *list 11
+            call {set_element}          // _ *kernel *list
 
             // populate list[12] with coinbase digest
-            dup 1 // _ *kernel *list *kernel
-            call {get_field_coinbase} // _ *kernel *list *coinbase
-            dup 2 // _ *kernel *list *coinbase *kernel
+            dup 1                       // _ *kernel *list *kernel
+            call {get_field_coinbase}   // _ *kernel *list *coinbase
+            dup 2                       // _ *kernel *list *coinbase *kernel
             call {get_field_size_coinbase} // _ *kernel *list *coinbase *coinbase_size
-            call {hash_varlen} // _ *kernel *list d4 d3 d2 d1 d0
-            dup 5 push 12 // _ *kernel *list d4 d3 d2 d1 d0 *list 12
-            call {set_element} // _ *kernel *list
+            call {hash_varlen}          // _ *kernel *list d4 d3 d2 d1 d0
+            dup 5 push 12               // _ *kernel *list d4 d3 d2 d1 d0 *list 12
+            call {set_element}          // _ *kernel *list
 
             // populate list[13] with timestamp digest
-            dup 1 // _ *kernel *list *kernel
-            call {get_field_timestamp} // _ *kernel *list *timestamp
+            dup 1                       // _ *kernel *list *kernel
+            call {get_field_timestamp}  // _ *kernel *list *timestamp
             dup 0 // _ *kernel *list *timestamp *timestamp
             call {get_field_size_timestamp} // _ *kernel *list *timestamp *timestamp_size
-            call {hash_varlen} // _ *kernel *list d4 d3 d2 d1 d0
-            dup 5 push 13 // _ *kernel *list d4 d3 d2 d1 d0 *list 13
-            call {set_element} // _ *kernel *list
+            call {hash_varlen}          // _ *kernel *list d4 d3 d2 d1 d0
+            dup 5 push 13               // _ *kernel *list d4 d3 d2 d1 d0 *list 13
+            call {set_element}          // _ *kernel *list
 
             // populate list[14] with mutator set hash digest
-            dup 1 // _ *kernel *list *kernel
+            dup 1                       // _ *kernel *list *kernel
             call {get_field_mutator_set_hash} // _ *kernel *list *mutator_set_hash
-            dup 2 // _ *kernel *list *mutator_set_hash *kernel
+            dup 2                       // _ *kernel *list *mutator_set_hash *kernel
             call {get_field_size_mutator_set_hash} // _ *kernel *list *mutator_set_hash *mutator_set_hash_size
-            call {hash_varlen} // _ *kernel *list d4 d3 d2 d1 d0
-            dup 5 push 14 // _ *kernel *list d4 d3 d2 d1 d0 *list 14
-            call {set_element} // _ *kernel *list
+            call {hash_varlen}          // _ *kernel *list d4 d3 d2 d1 d0
+            dup 5 push 14               // _ *kernel *list d4 d3 d2 d1 d0 *list 14
+            call {set_element}          // _ *kernel *list
 
             // populate list[15] with default digest
             push 0 push 0 push 0 push 0 push 0
-            dup 5 push 15 // _ *kernel *list d4 d3 d2 d1 d0 *list 15
-            call {set_element} // _ *kernel *list
+            dup 5 push 15               // _ *kernel *list d4 d3 d2 d1 d0 *list 15
+            call {set_element}          // _ *kernel *list
 
             // hash 14||15 and store in 7
-            dup 0 push 15 // _ *kernel *list *list 15
-            call {get_element} // _ *kernel *list d4 d3 d2 d1 d0
-            dup 5 push 14 // _ *kernel *list d4 d3 d2 d1 d0 *list 14
-            call {get_element} // _ *kernel *list d4 d3 d2 d1 d0 e4 e3 e2 e1 e0
-            hash // _ *kernel *list f4 f3 f2 f1 f0 0 0 0 0 0
-            pop pop pop pop pop // _ *kernel *list f4 f3 f2 f1 f0
-            dup 5 push 7 // _ *kernel *list f4 f3 f2 f1 f0 *list 7
+            dup 0 push 15               // _ *kernel *list *list 15
+            call {get_element}          // _ *kernel *list d4 d3 d2 d1 d0
+            dup 5 push 14               // _ *kernel *list d4 d3 d2 d1 d0 *list 14
+            call {get_element}          // _ *kernel *list d4 d3 d2 d1 d0 e4 e3 e2 e1 e0
+            hash                        // _ *kernel *list f4 f3 f2 f1 f0 0 0 0 0 0
+            pop pop pop pop pop         // _ *kernel *list f4 f3 f2 f1 f0
+            dup 5 push 7                // _ *kernel *list f4 f3 f2 f1 f0 *list 7
             call {set_element}
 
             // hash 12||13 and store in 6
-            dup 0 push 13 // _ *kernel *list *list 13
-            call {get_element} // _ *kernel *list d4 d3 d2 d1 d0
-            dup 5 push 12 // _ *kernel *list d4 d3 d2 d1 d0 *list 12
-            call {get_element} // _ *kernel *list d4 d3 d2 d1 d0 e4 e3 e2 e1 e0
-            hash // _ *kernel *list f4 f3 f2 f1 f0 0 0 0 0 0
-            pop pop pop pop pop // _ *kernel *list f4 f3 f2 f1 f0
-            dup 5 push 6 // _ *kernel *list f4 f3 f2 f1 f0 *list 6
+            dup 0 push 13               // _ *kernel *list *list 13
+            call {get_element}          // _ *kernel *list d4 d3 d2 d1 d0
+            dup 5 push 12               // _ *kernel *list d4 d3 d2 d1 d0 *list 12
+            call {get_element}          // _ *kernel *list d4 d3 d2 d1 d0 e4 e3 e2 e1 e0
+            hash                        // _ *kernel *list f4 f3 f2 f1 f0 0 0 0 0 0
+            pop pop pop pop pop         // _ *kernel *list f4 f3 f2 f1 f0
+            dup 5 push 6                // _ *kernel *list f4 f3 f2 f1 f0 *list 6
             call {set_element}
 
             // hash 10||11 and store in 5
-            dup 0 push 11 // _ *kernel *list *list 11
-            call {get_element} // _ *kernel *list d4 d3 d2 d1 d0
-            dup 5 push 10 // _ *kernel *list d4 d3 d2 d1 d0 *list 10
-            call {get_element} // _ *kernel *list d4 d3 d2 d1 d0 e4 e3 e2 e1 e0
-            hash // _ *kernel *list f4 f3 f2 f1 f0 0 0 0 0 0
-            pop pop pop pop pop // _ *kernel *list f4 f3 f2 f1 f0
-            dup 5 push 5 // _ *kernel *list f4 f3 f2 f1 f0 *list 5
+            dup 0 push 11               // _ *kernel *list *list 11
+            call {get_element}          // _ *kernel *list d4 d3 d2 d1 d0
+            dup 5 push 10               // _ *kernel *list d4 d3 d2 d1 d0 *list 10
+            call {get_element}          // _ *kernel *list d4 d3 d2 d1 d0 e4 e3 e2 e1 e0
+            hash                        // _ *kernel *list f4 f3 f2 f1 f0 0 0 0 0 0
+            pop pop pop pop pop         // _ *kernel *list f4 f3 f2 f1 f0
+            dup 5 push 5                // _ *kernel *list f4 f3 f2 f1 f0 *list 5
             call {set_element}
 
             // hash 8||9 and store in 4
-            dup 0 push 9 // _ *kernel *list *list 9
-            call {get_element} // _ *kernel *list d4 d3 d2 d1 d0
-            dup 5 push 8 // _ *kernel *list d4 d3 d2 d1 d0 *list 8
-            call {get_element} // _ *kernel *list d4 d3 d2 d1 d0 e4 e3 e2 e1 e0
-            hash // _ *kernel *list f4 f3 f2 f1 f0 0 0 0 0 0
-            pop pop pop pop pop // _ *kernel *list f4 f3 f2 f1 f0
-            dup 5 push 4 // _ *kernel *list f4 f3 f2 f1 f0 *list 4
+            dup 0 push 9                // _ *kernel *list *list 9
+            call {get_element}          // _ *kernel *list d4 d3 d2 d1 d0
+            dup 5 push 8                // _ *kernel *list d4 d3 d2 d1 d0 *list 8
+            call {get_element}          // _ *kernel *list d4 d3 d2 d1 d0 e4 e3 e2 e1 e0
+            hash                        // _ *kernel *list f4 f3 f2 f1 f0 0 0 0 0 0
+            pop pop pop pop pop         // _ *kernel *list f4 f3 f2 f1 f0
+            dup 5 push 4                // _ *kernel *list f4 f3 f2 f1 f0 *list 4
             call {set_element}
 
             // hash 6||7 and store in 3
-            dup 0 push 7 // _ *kernel *list *list 7
-            call {get_element} // _ *kernel *list d4 d3 d2 d1 d0
-            dup 5 push 6 // _ *kernel *list d4 d3 d2 d1 d0 *list 6
-            call {get_element} // _ *kernel *list d4 d3 d2 d1 d0 e4 e3 e2 e1 e0
-            hash // _ *kernel *list f4 f3 f2 f1 f0 0 0 0 0 0
-            pop pop pop pop pop // _ *kernel *list f4 f3 f2 f1 f0
-            dup 5 push 3 // _ *kernel *list f4 f3 f2 f1 f0 *list 3
+            dup 0 push 7                // _ *kernel *list *list 7
+            call {get_element}          // _ *kernel *list d4 d3 d2 d1 d0
+            dup 5 push 6                // _ *kernel *list d4 d3 d2 d1 d0 *list 6
+            call {get_element}          // _ *kernel *list d4 d3 d2 d1 d0 e4 e3 e2 e1 e0
+            hash                        // _ *kernel *list f4 f3 f2 f1 f0 0 0 0 0 0
+            pop pop pop pop pop         // _ *kernel *list f4 f3 f2 f1 f0
+            dup 5 push 3                // _ *kernel *list f4 f3 f2 f1 f0 *list 3
             call {set_element}
 
             // hash 4||5 and store in 2
-            dup 0 push 5 // _ *kernel *list *list 5
-            call {get_element} // _ *kernel *list d4 d3 d2 d1 d0
-            dup 5 push 4 // _ *kernel *list d4 d3 d2 d1 d0 *list 4
-            call {get_element} // _ *kernel *list d4 d3 d2 d1 d0 e4 e3 e2 e1 e0
-            hash // _ *kernel *list f4 f3 f2 f1 f0 0 0 0 0 0
-            pop pop pop pop pop // _ *kernel *list f4 f3 f2 f1 f0
-            dup 5 push 2 // _ *kernel *list f4 f3 f2 f1 f0 *list 2
+            dup 0 push 5                // _ *kernel *list *list 5
+            call {get_element}          // _ *kernel *list d4 d3 d2 d1 d0
+            dup 5 push 4                // _ *kernel *list d4 d3 d2 d1 d0 *list 4
+            call {get_element}          // _ *kernel *list d4 d3 d2 d1 d0 e4 e3 e2 e1 e0
+            hash                        // _ *kernel *list f4 f3 f2 f1 f0 0 0 0 0 0
+            pop pop pop pop pop         // _ *kernel *list f4 f3 f2 f1 f0
+            dup 5 push 2                // _ *kernel *list f4 f3 f2 f1 f0 *list 2
             call {set_element}
 
             // hash 2||3 and store in 1
-            dup 0 push 3 // _ *kernel *list *list 3
-            call {get_element} // _ *kernel *list d4 d3 d2 d1 d0
-            dup 5 push 2 // _ *kernel *list d4 d3 d2 d1 d0 *list 2
-            call {get_element} // _ *kernel *list d4 d3 d2 d1 d0 e4 e3 e2 e1 e0
-            hash // _ *kernel *list f4 f3 f2 f1 f0 0 0 0 0 0
-            pop pop pop pop pop // _ *kernel *list f4 f3 f2 f1 f0
-            dup 5 push 1 // _ *kernel *list f4 f3 f2 f1 f0 *list 1
+            dup 0 push 3                // _ *kernel *list *list 3
+            call {get_element}          // _ *kernel *list d4 d3 d2 d1 d0
+            dup 5 push 2                // _ *kernel *list d4 d3 d2 d1 d0 *list 2
+            call {get_element}          // _ *kernel *list d4 d3 d2 d1 d0 e4 e3 e2 e1 e0
+            hash                        // _ *kernel *list f4 f3 f2 f1 f0 0 0 0 0 0
+            pop pop pop pop pop         // _ *kernel *list f4 f3 f2 f1 f0
+            dup 5 push 1                // _ *kernel *list f4 f3 f2 f1 f0 *list 1
 
             call {set_element}
 
             // return list[1]
-            swap 1 // _ *list *kernel
+            swap 1                      // _ *list *kernel
             pop
             push 1 // _ *list 1
-            call {get_element} // _ d4 d3 d2 d1 d0
+            call {get_element}          // _ d4 d3 d2 d1 d0
 
             return
             "
