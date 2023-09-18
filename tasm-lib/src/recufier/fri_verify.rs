f1536cac3d1cc13d88de7a1eb568a27448f106e3
Alan Szepieniec <alan@neptune.cash>
2023-09-18 13:13:54 +0200

    include `FriVerify` object on stack

--- tasm-lib/src/recufier/fri_verify.rs
+++ tasm-lib/src/recufier/fri_verify.rs
@@ -16,10 +16,11 @@ use triton_vm::{
     fri::{Fri, FriValidationError},
     instruction::LabelledInstruction,
     proof_stream::ProofStream,
-    BFieldElement, NonDeterminism,
+    triton_asm, BFieldElement, NonDeterminism,
 };
 use twenty_first::{
     shared_math::{
+        bfield_codec::BFieldCodec,
         ntt::{intt, ntt},
         other::log_2_ceil,
         polynomial::Polynomial,
@@ -43,11 +44,14 @@ use crate::{
 /// The test succeeds with probability 1 if the codeword is of low degree; and with
 /// probability *soundness error* if the codeword is far from low-degree. If the test is
 /// not successful, the VM crashes.
+#[derive(Debug, Clone, PartialEq, Eq, BFieldCodec, TasmObject)]
 pub struct FriVerify {
     // expansion factor = 1 / rate
     pub expansion_factor: u32,
-    pub colinearity_checks_count: u32,
-    pub domain: ArithmeticDomain,
+    pub num_colinearity_checks: u32,
+    pub domain_length: u32,
+    pub domain_offset: BFieldElement,
+    domain_generator: BFieldElement,
 }
 
 impl FriVerify {
@@ -55,13 +59,15 @@ impl FriVerify {
         offset: BFieldElement,
         domain_length: u32,
         expansion_factor: u32,
-        colinearity_checks_count: u32,
+        num_colinearity_checks: u32,
     ) -> Self {
         let domain = ArithmeticDomain::of_length(domain_length as usize).with_offset(offset);
         Self {
-            domain,
             expansion_factor,
-            colinearity_checks_count,
+            num_colinearity_checks,
+            domain_length,
+            domain_offset: domain.offset,
+            domain_generator: domain.generator,
         }
     }
 
@@ -74,21 +80,28 @@ impl FriVerify {
             .unwrap()
     }
 
-    /// Computes the number of rounds and the max. degree of the last codeword
-    fn num_rounds(&self) -> (u8, u32) {
-        let max_degree = (self.domain.length / self.expansion_factor as usize) - 1;
-        let mut rounds_count = log_2_ceil(max_degree as u128 + 1) as u8;
-        let mut max_degree_of_last_round = 0u32;
-        if self.expansion_factor < self.colinearity_checks_count {
-            let num_missed_rounds = log_2_ceil(
-                (self.colinearity_checks_count as f64 / self.expansion_factor as f64).ceil()
-                    as u128,
-            ) as u8;
-            rounds_count -= num_missed_rounds;
-            max_degree_of_last_round = 2u32.pow(num_missed_rounds as u32) - 1;
-        }
+    /// Computes the number of rounds
+    pub fn num_rounds(&self) -> usize {
+        let first_round_code_dimension = self.first_round_max_degree() + 1;
+        let max_num_rounds = log_2_ceil(first_round_code_dimension as u128);
 
-        (rounds_count, max_degree_of_last_round)
+        // Skip rounds for which Merkle tree verification cost exceeds arithmetic cost,
+        // because more than half the codeword's locations are queried.
+        let num_rounds_checking_all_locations = self.num_colinearity_checks.ilog2() as u64;
+        let num_rounds_checking_most_locations = num_rounds_checking_all_locations + 1;
+
+        max_num_rounds.saturating_sub(num_rounds_checking_most_locations) as usize
+    }
+
+    /// Computes the max degree of the codeword interpolant after the last round
+    pub fn last_round_max_degree(&self) -> usize {
+        self.first_round_max_degree() >> self.num_rounds()
+    }
+
+    // Computes the max degree of the very first codeword interpolant
+    pub fn first_round_max_degree(&self) -> usize {
+        assert!(self.domain_length >= self.expansion_factor);
+        (self.domain_length / self.expansion_factor) as usize - 1
     }
 
     /// Compute a new list containing the `XFieldElement`s of the given list, but lifted
@@ -103,7 +116,7 @@ impl FriVerify {
     /// method is called `get_evaluation_argument`.
     /// TODO: write snippet for me
     fn get_colinearity_check_x(&self, idx: u32, round: usize) -> XFieldElement {
-        let domain_value = self.domain.offset * self.domain.generator.mod_pow_u32(idx);
+        let domain_value = self.domain_offset * self.domain_generator.mod_pow_u32(idx);
         let round_exponent = 2u32.pow(round as u32);
         let evaluation_argument = domain_value.mod_pow_u32(round_exponent);
 
@@ -122,8 +135,8 @@ impl FriVerify {
         let mut num_nondeterministic_digests_read = 0;
 
         // calculate number of rounds
-        let (num_rounds, max_degree_of_last_round) = self.num_rounds();
-        let num_rounds = num_rounds as usize;
+        let num_rounds = self.num_rounds();
+        let last_round_max_degree = self.last_round_max_degree();
 
         // Extract all roots and calculate alpha based on Fiat-Shamir challenge
         let mut roots = Vec::with_capacity(num_rounds);
@@ -163,16 +176,15 @@ impl FriVerify {
         let mut last_polynomial = last_codeword.clone();
 
         let last_fri_domain_generator = self
-            .domain
-            .generator
+            .domain_generator
             .mod_pow_u32(2u32.pow(num_rounds as u32)); // <-- snippet me
         intt::<XFieldElement>(&mut last_polynomial, last_fri_domain_generator, log_2_of_n); // <-- snippet me
         let last_poly_degree = Polynomial::new(last_polynomial).degree(); // <-- snippet me
 
-        if last_poly_degree > max_degree_of_last_round as isize {
+        if last_poly_degree > last_round_max_degree as isize {
             println!(
                 "last_poly_degree is {last_poly_degree}, \
-                degree_of_last_round is {max_degree_of_last_round}",
+                degree_of_last_round is {last_round_max_degree}",
             );
             bail!(FriValidationError::LastIterationTooHighDegree)
         }
@@ -180,9 +192,9 @@ impl FriVerify {
         // Query phase
         // query step 0: get "A" indices and verify set membership of corresponding values.
         let mut a_indices =
-            proof_stream.sample_indices(self.domain.length as u32, self.colinearity_checks_count);
+            proof_stream.sample_indices(self.domain_length, self.num_colinearity_checks);
 
-        let tree_height = self.domain.length.ilog2() as usize;
+        let tree_height = self.domain_length.ilog2() as usize;
         let fri_response = proof_stream.dequeue().unwrap().as_fri_response().unwrap();
         assert_eq!(a_indices.len(), fri_response.revealed_leaves.len());
         let mut a_values = fri_response.revealed_leaves;
@@ -242,7 +254,7 @@ impl FriVerify {
         // set up "B" for offsetting inside loop.  Note that "B" and "A" indices can be calcuated
         // from each other.
         let mut b_indices = a_indices.clone();
-        let mut current_domain_len = self.domain.length as u32;
+        let mut current_domain_len = self.domain_length;
         let mut current_tree_height = tree_height as u32;
 
         // query step 1:  loop over FRI rounds, verify "B"s, compute values for "C"s
@@ -301,10 +313,10 @@ impl FriVerify {
                 }
             }
 
-            debug_assert_eq!(self.colinearity_checks_count, a_indices.len() as u32);
-            debug_assert_eq!(self.colinearity_checks_count, b_indices.len() as u32);
-            debug_assert_eq!(self.colinearity_checks_count, a_values.len() as u32);
-            debug_assert_eq!(self.colinearity_checks_count, b_values.len() as u32);
+            debug_assert_eq!(self.num_colinearity_checks, a_indices.len() as u32);
+            debug_assert_eq!(self.num_colinearity_checks, b_indices.len() as u32);
+            debug_assert_eq!(self.num_colinearity_checks, a_values.len() as u32);
+            debug_assert_eq!(self.num_colinearity_checks, b_values.len() as u32);
 
             if r == 0 {
                 // save other half of indices and revealed leafs of first round for returning
@@ -319,7 +331,7 @@ impl FriVerify {
             current_domain_len /= 2;
             current_tree_height -= 1;
             let c_indices = a_indices.iter().map(|x| x % current_domain_len).collect();
-            let c_values = (0..self.colinearity_checks_count as usize)
+            let c_values = (0..self.num_colinearity_checks as usize)
                 .map(|i| {
                     Polynomial::<XFieldElement>::get_colinear_y(
                         (self.get_colinearity_check_x(a_indices[i], r), a_values[i]),
@@ -337,7 +349,7 @@ impl FriVerify {
         // Finally compare "C" values (which are named "A" values in this enclosing scope) with
         // last codeword from the proofstream.
         a_indices = a_indices.iter().map(|x| x % current_domain_len).collect();
-        if !(0..self.colinearity_checks_count as usize)
+        if !(0..self.num_colinearity_checks as usize)
             .all(|i| last_codeword[a_indices[i] as usize] == a_values[i])
         {
             bail!(FriValidationError::MismatchingLastCodeword);
@@ -356,11 +368,11 @@ impl FriVerify {
     pub fn pseudorandom_fri_proof_stream(&self, seed: [u8; 32]) -> VmProofStream {
         let mut rng: StdRng = SeedableRng::from_seed(seed);
 
-        let max_degree = self.domain.length / self.expansion_factor as usize - 1;
+        let max_degree = self.domain_length as usize / self.expansion_factor as usize - 1;
         let coefficients: Vec<XFieldElement> = (0..=max_degree).map(|_| rng.gen()).collect();
         let mut codeword = [
             coefficients,
-            vec![XFieldElement::zero(); self.domain.length - max_degree - 1],
+            vec![XFieldElement::zero(); self.domain_length as usize - max_degree - 1],
         ]
         .concat();
         let n = codeword.len();
@@ -372,10 +384,10 @@ impl FriVerify {
         let mut proof_stream = ProofStream::<VmHasher>::new();
 
         let fri = Fri::new(
-            self.domain.offset,
-            self.domain.length,
+            self.domain_offset,
+            self.domain_length as usize,
             self.expansion_factor as usize,
-            self.colinearity_checks_count as usize,
+            self.num_colinearity_checks as usize,
         );
         fri.prove(&codeword, &mut proof_stream);
 
@@ -385,7 +397,10 @@ impl FriVerify {
 
 impl BasicSnippet for FriVerify {
     fn inputs(&self) -> Vec<(DataType, String)> {
-        vec![(DataType::VoidPointer, "*proof_stream".to_string())]
+        vec![
+            (DataType::VoidPointer, "*proof_stream".to_string()),
+            (DataType::VoidPointer, "*fri_verify".to_string()),
+        ]
     }
 
     fn outputs(&self) -> Vec<(DataType, String)> {
@@ -406,7 +421,13 @@ impl BasicSnippet for FriVerify {
     }
 
     fn code(&self, _library: &mut Library) -> Vec<LabelledInstruction> {
-        todo!()
+        let entrypoint = self.entrypoint();
+        triton_asm! {
+            // BEFORE: _ *proof_stream *fri_verify
+            // AFTER: _ *proof_stream *indices_and_leafs
+            {entrypoint}:
+
+        }
     }
 }
 
@@ -419,6 +440,11 @@ impl Procedure for FriVerify {
         _public_input: &[triton_vm::BFieldElement],
         sponge_state: &mut crate::VmHasherState,
     ) -> Vec<triton_vm::BFieldElement> {
+        // read fri object
+        let fri_pointer = stack.pop().unwrap();
+        let fri_verify = *FriVerify::decode_from_memory(memory, fri_pointer).unwrap();
+        assert_eq!(fri_verify, *self);
+
         // read proof stream
         let proof_stream_pointer = stack.pop().unwrap();
         let mut proof_stream: VmProofStream =
@@ -460,10 +486,12 @@ impl Procedure for FriVerify {
         let mut stack = get_init_tvm_stack();
         let mut memory: HashMap<BFieldElement, BFieldElement> = HashMap::new();
         let proof_stream_pointer = load_to_memory(&mut memory, proof_stream);
+        let fri_verify_pointer = load_to_memory(&mut memory, self.clone());
         let nondeterminism = NonDeterminism::new(vec![])
             .with_ram(memory.clone())
             .with_digests(digests);
         stack.push(proof_stream_pointer);
+        stack.push(fri_verify_pointer);
         let stdin = vec![];
         let sponge_state = VmHasherState::new(Domain::VariableLength);
 
@@ -487,7 +515,7 @@ mod test {
     use super::FriVerify;
 
     #[test]
-    fn num_rounds_match() {
+    fn fri_derived_params_match() {
         let mut rng = thread_rng();
         let expansion_factor = 1 << (rng.next_u32() % 17);
         let colinearity_checks_count = rng.next_u32() % 100;
@@ -501,14 +529,22 @@ mod test {
             colinearity_checks_count,
         );
 
-        let (num_rounds, last_max_degree) = fri_verify.num_rounds();
-        assert_eq!(
-            ((last_max_degree + 1) << num_rounds) * expansion_factor,
-            domain_length
+        let fri = Fri::<VmHasher>::new(
+            offset,
+            domain_length as usize,
+            expansion_factor as usize,
+            colinearity_checks_count as usize,
         );
 
-        // fri.num_rounds() is private
-        // assert_eq!(fri_verify.num_rounds(), fri.num_rounds());
+        assert_eq!(fri_verify.num_rounds(), fri.num_rounds());
+        assert_eq!(
+            fri_verify.last_round_max_degree(),
+            fri.last_round_max_degree()
+        );
+        assert_eq!(
+            fri_verify.first_round_max_degree(),
+            fri.first_round_max_degree()
+        );
     }
 
     #[test]
