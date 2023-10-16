975021c60489b3811ba14d29f191b4b0da82b6c9
Ameya <f20200139@pilani.bits-pilani.ac.in>
2023-10-16 11:57:42 +0530

    get_colinear_y -- done

--- tasm-lib/src/recufier/get_colinear_y.rs
+++ tasm-lib/src/recufier/get_colinear_y.rs
@@ -54,13 +54,19 @@ impl BasicSnippet for ColinearYXfe {
                 // stack: _ p2x2 p2x1 p2x0 p0x2 p0x1 p0x0 p1x2 p1x1 p1x0 p0y2 p0y1 p0y0 p1y2 p1y1 p1y0 p0y2 p0y1 p0y0
                 push -1
                 xbmul
-                xxmul
+                xxadd
                 push -1
                 xbmul
                 // dy = p0.y-p1.y
+                // stack: _ p2x2 p2x1 p2x0 p0x2 p0x1 p0x0 p1x2 p1x1 p1x0 p0y2 p0y1 p0y0 p1y2 p1y1 p1y0 dy2 dy1 dy0
+                swap 3 pop
+                // stack: _ p2x2 p2x1 p2x0 p0x2 p0x1 p0x0 p1x2 p1x1 p1x0 p0y2 p0y1 p0y0 p1y2 p1y1 dy0 dy2 dy1
+                swap 3 pop
+                // stack: _ p2x2 p2x1 p2x0 p0x2 p0x1 p0x0 p1x2 p1x1 p1x0 p0y2 p0y1 p0y0 p1y2 dy1 dy0 dy2
+                swap 3 pop
                 // stack: _ p2x2 p2x1 p2x0 p0x2 p0x1 p0x0 p1x2 p1x1 p1x0 p0y2 p0y1 p0y0 dy2 dy1 dy0
                 swap 6
-                // stack: _ p2x2 p2x1 p2x0 p0x2 p0x1 p0x0 p1x2 p1x1 dy0 p0y2 p0y1 p0y0 dy2 dy1 p1x0
+
                 swap 1
                 // stack: _ p2x2 p2x1 p2x0 p0x2 p0x1 p0x0 p1x2 p1x1 dy0 p0y2 p0y1 p0y0 dy2 p1x0 dy1
                 swap 7
@@ -75,10 +81,14 @@ impl BasicSnippet for ColinearYXfe {
                 // stack: _ p2x2 p2x1 p2x0 p0x2 p0x1 p0x0 dy2 dy1 dy0 p0y2 p0y1 p0y0 p1x2 p1x1 p1x0 p0x2 p0x1 p0x0
                 push -1
                 xbmul
-                xxmul
+                xxadd
                 push -1
                 xbmul
                 // dx = p0.x-p1.x
+                // stack: _ p2x2 p2x1 p2x0 p0x2 p0x1 p0x0 dy2 dy1 dy0 p0y2 p0y1 p0y0 p1x2 p1x1 p1x0 dx2 dx1 dx0
+                swap 3 pop
+                swap 3 pop
+                swap 3 pop
                 // stack: _ p2x2 p2x1 p2x0 p0x2 p0x1 p0x0 dy2 dy1 dy0 p0y2 p0y1 p0y0 dx2 dx1 dx0
                 swap 12 swap 1 swap 13 swap 2 swap 14 swap 2 swap 1
                 // stack: _ dx2 dx1 dx0 p0x2 p0x1 p0x0 dy2 dy1 dy0 p0y2 p0y1 p0y0 p2x2 p2x1 p2x0
@@ -91,15 +101,24 @@ impl BasicSnippet for ColinearYXfe {
                 push -1
                 xbmul
                 xxadd
+                 // stack: _ p0y2 p0y1 p0y0 dx2 dx1 dx0 dy2 dy1 dy0 p2x2 p2x1 p2x0 (p2.x-p0.x)_2 (p2.x-p0.x)_1 (p2.x-p0.x)_0
+                swap 3 pop
+                swap 3 pop
+                swap 3 pop
+                // stack: _ p0y2 p0y1 p0y0 dx2 dx1 dx0 dy2 dy1 dy0 (p2.x-p0.x)_2 (p2.x-p0.x)_1 (p2.x-p0.x)_0
                 xxmul
                 // a = (p2.x-p0.x)*dy
-                // stack: _ p0y2 p0y1 p0y0 dx2 dx1 dx0 a2 a1 a0
-                swap 9 swap 1 swap 10 swap 2 swap 11 swap 2 swap 1
+                // stack: _ p0y2 p0y1 p0y0 dx2 dx1 dx0 dy2 dy1 dy0 a2 a1 a0
+                swap 3 pop swap 3 pop swap 3 pop
+                  // stack: _ p0y2 p0y1 p0y0 dx2 dx1 dx0 a2 a1 a0
+                swap 6 swap 1 swap 7 swap 2 swap 8 swap 2 swap 1
                 // stack: _ a2 a1 a0 dx2 dx1 dx0 p0y2 p0y1 p0y0
                 dup 5 dup 5 dup 5
                 // stack: _ a2 a1 a0 dx2 dx1 dx0 p0y2 p0y1 p0y0 dx2 dx1 dx0
                 xxmul
                 // b = (p0.y)*dx
+                // stack: _ a2 a1 a0 dx2 dx1 dx0 p0y2 p0y1 p0y0 b2 b1 b0
+                swap 3 pop swap 3 pop swap 3 pop
                 // stack: _ a2 a1 a0 dx2 dx1 dx0 b2 b1 b0
                 swap 3 swap 1 swap 4 swap 2 swap 5 swap 2 swap 1
                 // stack: _ a2 a1 a0 b2 b1 b0 dx2 dx1 dx0
@@ -107,12 +126,16 @@ impl BasicSnippet for ColinearYXfe {
                 // stack: _ dx2 dx1 dx0 b2 b1 b0 a2 a1 a0
                 xxadd
                 // c = a+b
+                // stack: _ dx2 dx1 dx0 b2 b1 b0 c2 c1 c0
+                swap 3 pop swap 3 pop swap 3 pop
                 // stack: _ dx2 dx1 dx0 c2 c1 c0
                 swap 3 swap 1 swap 4 swap 2 swap 5 swap 2 swap 1
                 // stack: _ c2 c1 c0 dx2 dx1 dx0
                 xinvert
-                mul
+                xxmul
                 // p2.y = c/dx
+                // stack: _ c2 c1 c0 p2y2 p2y1 p2y0
+                swap 3 pop swap 3 pop swap 3 pop
                 // stack: _ p2y2 p2y1 p2y0
                 return
         )
