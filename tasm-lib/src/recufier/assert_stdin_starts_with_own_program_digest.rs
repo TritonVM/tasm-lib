d7b3b6170ca1a10ed50cf4fc65c2c00859a995b9
Thorkil Vaerge <thor@neptune.cash>
2024-01-29 16:28:06 +0100

    Fix bug in assumption about TVM's initial stack
    Co-authored-by: Ferdinand Sauer <ferdinand@neptune.cash>

--- tasm-lib/src/recufier/assert_stdin_starts_with_own_program_digest.rs
+++ tasm-lib/src/recufier/assert_stdin_starts_with_own_program_digest.rs
@@ -44,10 +44,6 @@ impl BasicSnippet for AssertStdInStartsWithOwnProgramDigest {
 
 #[cfg(test)]
 mod tests {
-    use num_traits::Zero;
-    use triton_vm::op_stack::NUM_OP_STACK_REGISTERS;
-    use triton_vm::twenty_first::shared_math::tip5::DIGEST_LENGTH;
-
     use super::*;
     use crate::execute_with_terminal_state;
     use crate::library::Library;
@@ -70,11 +66,7 @@ mod tests {
 
         let program = Program::new(&code_for_test);
         let program_digest = program.hash::<VmHasher>();
-        let init_stack = [
-            program_digest.values().to_vec(),
-            vec![BFieldElement::zero(); NUM_OP_STACK_REGISTERS - DIGEST_LENGTH],
-        ]
-        .concat();
+        let init_stack = snippet.init_stack_for_isolated_run();
 
         ProgramSetup {
             program,
@@ -87,7 +79,7 @@ mod tests {
     fn positive_test() {
         let test_setup = test_program();
 
-        let std_in = test_setup.program_digest.encode();
+        let std_in = test_setup.program_digest.reversed().encode();
         let vm_end_state = execute_with_terminal_state(
             &test_setup.program,
             &std_in,
--- tasm-lib/src/traits/basic_snippet.rs
+++ tasm-lib/src/traits/basic_snippet.rs
@@ -1,13 +1,46 @@
-use triton_vm::instruction::LabelledInstruction;
+use num_traits::Zero;
+use triton_vm::op_stack::NUM_OP_STACK_REGISTERS;
+use triton_vm::prelude::*;
 
 use crate::data_type::DataType;
 use crate::library::Library;
+use crate::VmHasher;
 
 pub trait BasicSnippet {
     fn inputs(&self) -> Vec<(DataType, String)>;
     fn outputs(&self) -> Vec<(DataType, String)>;
     fn entrypoint(&self) -> String;
     fn code(&self, library: &mut Library) -> Vec<LabelledInstruction>;
+
+    fn link_for_isolated_run(&self) -> Vec<LabelledInstruction> {
+        let mut library = Library::empty();
+        let entrypoint = self.entrypoint();
+        let function_body = self.code(&mut library);
+        let library_code = library.all_imports();
+
+        // The TASM code is always run through a function call, so the 1st instruction is a call to the
+        // function in question.
+        let code = triton_asm!(
+            call {entrypoint}
+            halt
+
+            {&function_body}
+            {&library_code}
+        );
+
+        code
+    }
+
+    fn init_stack_for_isolated_run(&self) -> Vec<BFieldElement> {
+        let code = self.link_for_isolated_run();
+        let program = Program::new(&code);
+        let program_digest = program.hash::<VmHasher>();
+        [
+            program_digest.reversed().values().to_vec(),
+            vec![BFieldElement::zero(); NUM_OP_STACK_REGISTERS - tip5::DIGEST_LENGTH],
+        ]
+        .concat()
+    }
 
     fn stack_diff(&self) -> isize {
         let mut diff = 0isize;
@@ -18,5 +51,54 @@ pub trait BasicSnippet {
             diff += dt.stack_size() as isize;
         }
         diff
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use triton_vm::{program::Program, triton_asm};
+
+    use crate::traits::basic_snippet::BasicSnippet;
+
+    struct DummySnippet;
+
+    impl BasicSnippet for DummySnippet {
+        fn inputs(&self) -> Vec<(crate::data_type::DataType, String)> {
+            vec![]
+        }
+
+        fn outputs(&self) -> Vec<(crate::data_type::DataType, String)> {
+            vec![]
+        }
+
+        fn entrypoint(&self) -> String {
+            "____dummy_snippet_test".to_string()
+        }
+
+        fn code(
+            &self,
+            _library: &mut crate::library::Library,
+        ) -> Vec<triton_vm::prelude::LabelledInstruction> {
+            triton_asm!(
+                {self.entrypoint()}:
+                    push 14
+                    push 14
+                    pop 2
+                    return
+            )
+        }
+    }
+
+    #[test]
+    fn init_stack_agrees_with_tvm() {
+        // Verify that our assumptions about the initial stack at program start
+        // agrees with Triton VM.
+        let calculated_init_stack = DummySnippet.init_stack_for_isolated_run();
+        let program = DummySnippet.link_for_isolated_run();
+        let program = Program::new(&program);
+        let init_vm_state =
+            triton_vm::vm::VMState::new(&program, Default::default(), Default::default());
+
+        assert_eq!(init_vm_state.op_stack.stack, calculated_init_stack);
     }
 }
