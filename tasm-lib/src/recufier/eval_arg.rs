3cc28413a80ff18b87653bbab947dd6aeebd17e8
sword_smith <thor@neptune.cash>
2024-02-02 00:29:27 +0100

    Add snippet for fastest possible terminal calculations
    Requires the symbols list to be an array and requires that it's traversed
    in reverse order *and* that the size of the array is known at
    compile-time.

--- tasm-lib/src/recufier/eval_arg.rs
+++ tasm-lib/src/recufier/eval_arg.rs
@@ -1,1 +1,2 @@
 pub mod compute_terminal;
+pub mod compute_terminal_const_sized_reversed;
---
+++ tasm-lib/src/recufier/eval_arg/compute_terminal_const_sized_reversed.rs
@@ -0,0 +1,252 @@
+use crate::data_type::{ArrayType, DataType};
+use crate::traits::basic_snippet::BasicSnippet;
+use crate::Library;
+use triton_vm::prelude::*;
+
+/// A snippet for calculating terminal value by traversing the symbols
+/// list in reverse order and where symbols list length is statically
+/// known. Produces verbose code but minimizes clock cycle count.
+struct ComputeTerminalConstSizedReversed<const N: usize>;
+
+impl<const N: usize> BasicSnippet for ComputeTerminalConstSizedReversed<N> {
+    fn inputs(&self) -> Vec<(DataType, String)> {
+        vec![
+            (DataType::Xfe, "challenge".to_owned()),
+            (DataType::Xfe, "initial".to_owned()),
+            (
+                DataType::Array(Box::new(ArrayType {
+                    element_type: DataType::Bfe,
+                    length: N,
+                })),
+                "symbols".to_owned(),
+            ),
+        ]
+    }
+
+    fn outputs(&self) -> Vec<(DataType, String)> {
+        vec![(DataType::Xfe, "terminal".to_owned())]
+    }
+
+    fn entrypoint(&self) -> String {
+        format!("tasm_recufier_eval_arg_compute_terminal_const_sized_reversed_sym_len_{N}")
+    }
+
+    fn code(&self, _library: &mut Library) -> Vec<LabelledInstruction> {
+        assert!(
+            N > 0,
+            "Cannot generate code for const-symbols length of zero"
+        );
+
+        // _ [challenge] [initial] *symbols
+        let entrypoint = self.entrypoint();
+        let one_iteration = triton_asm!(
+            // _ [challenge] *symbol [initial]
+
+                dup 6
+                dup 6
+                dup 6
+                xxmul
+                // _ [challenge] *symbol [initial * challenge]
+
+                dup 3
+                read_mem 1
+                // _ [challenge] *symbol [initial * challenge] symbol *symbol'
+
+                swap 5
+                pop 1
+                // _ [challenge] *symbol' [initial * challenge] symbol
+
+                add
+                // _ [challenge] *previous_elem [initial * challenge + symbol]
+                // _ [challenge] *previous_elem [acc']
+        );
+        let iterations = vec![one_iteration; N].concat();
+        triton_asm!(
+            // _ [challenge] [initial] *symbols
+            {entrypoint}:
+                push {N - 1}
+                add
+                // _ [challenge] [initial] *last_symbol
+                // _ [challenge] init_2 init_1 init_0 *last_symbol
+
+                swap 3
+                // _ [challenge] *last_symbol init_1 init_0 init_2
+
+                swap 2
+                // _ [challenge] *last_symbol init_2 init_0 init_1
+
+                swap 1
+                // _ [challenge] *last_symbol init_2 init_1 init_0
+                // _ [challenge] *last_symbol [initial]
+
+                {&iterations}
+                 // _ [challenge] (*first_symbol - 1) [terminal]
+
+                swap 4 pop 1
+                swap 4 pop 1
+                swap 4 pop 1
+                // _ [terminal] (*first_symbol - 1)
+
+                pop 1
+                // _ [terminal]
+
+                return
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::snippet_bencher::BenchmarkCase;
+    use crate::traits::function::Function;
+    use crate::traits::function::FunctionInitialState;
+    use crate::traits::function::ShadowedFunction;
+    use crate::traits::rust_shadow::RustShadow;
+    use itertools::Itertools;
+    use num_traits::One;
+    use num_traits::Zero;
+    use rand::rngs::StdRng;
+    use rand::Rng;
+    use rand::SeedableRng;
+    use std::collections::HashMap;
+    use triton_vm::table::cross_table_argument::CrossTableArg;
+    use triton_vm::table::cross_table_argument::EvalArg;
+
+    impl<const N: usize> Function for ComputeTerminalConstSizedReversed<N> {
+        fn rust_shadow(
+            &self,
+            stack: &mut Vec<BFieldElement>,
+            memory: &mut std::collections::HashMap<BFieldElement, BFieldElement>,
+        ) {
+            let symbols_pointer = stack.pop().unwrap();
+            let initial = XFieldElement::new([
+                stack.pop().unwrap(),
+                stack.pop().unwrap(),
+                stack.pop().unwrap(),
+            ]);
+            let challenge = XFieldElement::new([
+                stack.pop().unwrap(),
+                stack.pop().unwrap(),
+                stack.pop().unwrap(),
+            ]);
+            let mut symbols_elem_pointer = symbols_pointer + BFieldElement::new(N as u64 - 1);
+
+            let mut symbols = vec![];
+            for _ in 0..N {
+                symbols.push(memory[&symbols_elem_pointer]);
+                symbols_elem_pointer.decrement()
+            }
+
+            let result = EvalArg::compute_terminal(&symbols, initial, challenge);
+
+            for elem in result.coefficients.into_iter().rev() {
+                stack.push(elem);
+            }
+        }
+
+        fn pseudorandom_initial_state(
+            &self,
+            seed: [u8; 32],
+            _bench_case: Option<BenchmarkCase>,
+        ) -> FunctionInitialState {
+            let mut rng: StdRng = SeedableRng::from_seed(seed);
+            let symbols_pointer = rng.gen();
+            let rand_initial = rng.gen();
+            let rand_challenge = rng.gen();
+            self.prepare_state(symbols_pointer, rand_initial, rand_challenge)
+        }
+
+        fn corner_case_initial_states(&self) -> Vec<FunctionInitialState> {
+            let mut rng: StdRng = SeedableRng::from_seed([101u8; 32]);
+            let symbols_pointer = rng.gen();
+            vec![
+                self.prepare_state(
+                    symbols_pointer,
+                    XFieldElement::zero(),
+                    XFieldElement::zero(),
+                ),
+                self.prepare_state(symbols_pointer, XFieldElement::one(), XFieldElement::zero()),
+                self.prepare_state(symbols_pointer, XFieldElement::zero(), XFieldElement::one()),
+                self.prepare_state(symbols_pointer, XFieldElement::one(), XFieldElement::one()),
+            ]
+        }
+    }
+
+    impl<const N: usize> ComputeTerminalConstSizedReversed<N> {
+        fn prepare_state(
+            &self,
+            symbols_pointer: BFieldElement,
+            initial: XFieldElement,
+            challenge: XFieldElement,
+        ) -> FunctionInitialState {
+            let initial = initial.coefficients.into_iter().rev().collect_vec();
+            let challenge = challenge.coefficients.into_iter().rev().collect_vec();
+
+            let mut init_memory = HashMap::default();
+            for i in 0..N {
+                let i = BFieldElement::new(i as u64);
+                init_memory.insert(symbols_pointer + i, i + BFieldElement::new(404040));
+            }
+            let init_stack = [
+                self.init_stack_for_isolated_run(),
+                challenge,
+                initial,
+                vec![symbols_pointer],
+            ]
+            .concat();
+
+            FunctionInitialState {
+                stack: init_stack,
+                memory: init_memory,
+            }
+        }
+    }
+
+    #[test]
+    fn eval_compute_terminal_test_rev_const_sized_1() {
+        ShadowedFunction::new(ComputeTerminalConstSizedReversed::<1>).test()
+    }
+
+    #[test]
+    fn eval_compute_terminal_test_rev_const_sized_2() {
+        ShadowedFunction::new(ComputeTerminalConstSizedReversed::<2>).test()
+    }
+
+    #[test]
+    fn eval_compute_terminal_test_rev_const_sized_3() {
+        ShadowedFunction::new(ComputeTerminalConstSizedReversed::<3>).test()
+    }
+
+    #[test]
+    fn eval_compute_terminal_test_rev_const_sized_4() {
+        ShadowedFunction::new(ComputeTerminalConstSizedReversed::<4>).test()
+    }
+
+    #[test]
+    fn eval_compute_terminal_test_rev_const_sized_34() {
+        ShadowedFunction::new(ComputeTerminalConstSizedReversed::<34>).test()
+    }
+
+    #[test]
+    fn eval_compute_terminal_test_rev_const_sized_64() {
+        ShadowedFunction::new(ComputeTerminalConstSizedReversed::<64>).test()
+    }
+}
+
+#[cfg(test)]
+mod bench {
+    use super::*;
+    use crate::traits::function::ShadowedFunction;
+    use crate::traits::rust_shadow::RustShadow;
+
+    #[test]
+    fn bench_const_sized_terminal_calc_rev_30() {
+        ShadowedFunction::new(ComputeTerminalConstSizedReversed::<30>).bench()
+    }
+
+    #[test]
+    fn bench_const_sized_terminal_calc_rev_100() {
+        ShadowedFunction::new(ComputeTerminalConstSizedReversed::<100>).bench()
+    }
+}
