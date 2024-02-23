02307cdcf07aa1010d78022458faa67135c921d6
Thorkil Vaerge <thor@neptune.cash>
2024-02-23 18:39:03 +0100

    Add snippet for summing XFE array

--- tasm-lib/src/rust_shadowing_helper_functions/array.rs
+++ tasm-lib/src/rust_shadowing_helper_functions/array.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 
-use triton_vm::twenty_first::shared_math::b_field_element::BFieldElement;
+use triton_vm::twenty_first::shared_math::{
+    b_field_element::BFieldElement, bfield_codec::BFieldCodec,
+};
 
 use crate::data_type::DataType;
 
@@ -14,6 +16,20 @@ pub fn insert_random_array(
 
     for element in random_values {
         for word in element {
+            memory.insert(array_pointer, word);
+            array_pointer.increment();
+        }
+    }
+}
+
+/// Insert a list of elements into memory encoded as if the list was an array
+pub fn insert_as_array<T: BFieldCodec>(
+    mut array_pointer: BFieldElement,
+    memory: &mut HashMap<BFieldElement, BFieldElement>,
+    array: Vec<T>,
+) {
+    for element in array {
+        for word in element.encode() {
             memory.insert(array_pointer, word);
             array_pointer.increment();
         }
