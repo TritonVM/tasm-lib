c4cebc8eca8534690addf7748e886468c8458b74
danda <dan-da@users.noreply.github.com>
2024-03-18 13:08:38 -0700

    chore: mmra_with_mps has moved in twenty-first
    fix #90

--- tasm-lib/src/mmr/verify_from_memory.rs
+++ tasm-lib/src/mmr/verify_from_memory.rs
@@ -7,8 +7,8 @@ use rand::Rng;
 use triton_vm::prelude::*;
 use triton_vm::twenty_first::prelude::AlgebraicHasher;
 use triton_vm::twenty_first::prelude::MmrMembershipProof;
-use triton_vm::twenty_first::test_shared::mmr::mmra_with_mps;
 use twenty_first::shared_math::other::random_elements;
+use twenty_first::util_types::mmr::mmr_accumulator::util::mmra_with_mps;
 use twenty_first::util_types::mmr::mmr_accumulator::MmrAccumulator;
 use twenty_first::util_types::mmr::mmr_trait::Mmr;
 
