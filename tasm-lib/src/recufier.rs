c5642f395301a8a2305609df8e4131c2528e3ca8
Thorkil Vaerge <thor@neptune.cash>
2024-03-29 11:46:35 +0100

    refactor!: Move proof iterator struct to own directory
    Also adds a snippet to generate a new instance of this struct, and gives
    the struct an explicit type. Previously the type was implicit.

--- tasm-lib/src/recufier.rs
+++ tasm-lib/src/recufier.rs
@@ -6,9 +6,9 @@ pub mod get_collinear_y;
 pub mod get_collinearity_check_x;
 pub mod master_ext_table;
 pub mod own_program_digest;
-pub mod proof_stream;
 pub mod read_and_verify_own_program_digest_from_std_in;
 #[cfg(test)]
 mod standalone_fri_verify;
 pub mod verify_authentication_paths_for_leaf_and_index_list;
+pub mod vm_proof_iter;
 pub mod xfe_ntt;
