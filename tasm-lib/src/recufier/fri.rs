f6ddcc925da548e65dd99e2a6b682ce8bbe6cf74
Thorkil Vaerge <thor@neptune.cash>
2024-03-29 14:57:13 +0100

    fixup: Remove not-yet-included source code files

--- tasm-lib/src/recufier/fri.rs
+++ tasm-lib/src/recufier/fri.rs
@@ -1,6 +1,6 @@
 pub mod collinear_y;
 pub mod collinearity_check_x;
-pub mod derive_from_stark_params;
+// pub mod derive_from_stark_params;
 #[cfg(test)]
 mod standalone_fri_verify;
 pub mod verify;
