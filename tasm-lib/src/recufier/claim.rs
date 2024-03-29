6eb531b5cd6dcc2464606e8b3a1e946e8791aa65
Thorkil Vaerge <thor@neptune.cash>
2024-03-29 11:59:39 +0100

    refactor!: Move claim and fri verify type for better code reuse

--- tasm-lib/src/recufier/claim.rs
+++ tasm-lib/src/recufier/claim.rs
@@ -1,1 +1,2 @@
 pub mod instantiate_fiat_shamir_with_claim;
+pub mod shared;
---
+++ tasm-lib/src/recufier/claim/shared.rs
@@ -0,0 +1,15 @@
+use crate::data_type::{DataType, StructType};
+
+// TODO: This *must* match the type of `Claim` used in Triton VM. It would be preferable if
+// it could be derived/generated from that type at the source.
+// The tests below *should* verify that this type is consistent with the one used in TVM.
+pub(crate) fn claim_type() -> StructType {
+    StructType {
+        name: "Claim".to_owned(),
+        fields: vec![
+            ("program_digest".to_owned(), DataType::Digest),
+            ("input".to_owned(), DataType::List(Box::new(DataType::Bfe))),
+            ("output".to_owned(), DataType::List(Box::new(DataType::Bfe))),
+        ],
+    }
+}
