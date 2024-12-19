0ea12d2904bfe3d2cf470a30d15069206830ebfe
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2024-12-19 15:00:16 +0100

    signoff!: u64::div2
    BREAKING CHANGE: Remove the implementation of `DeprecatedSnippet` for
    `u64::div2::Div2`, implement `BasicSnippet` directly.

--- tasm-lib/src/arithmetic/u64/div2.rs
+++ tasm-lib/src/arithmetic/u64/div2.rs
@@ -1,237 +1,176 @@
 use std::collections::HashMap;
 
-use num::Zero;
-use rand::prelude::*;
 use triton_vm::prelude::*;
-use twenty_first::prelude::U32s;
 
-use crate::empty_stack;
 use crate::prelude::*;
-use crate::push_encodable;
-use crate::traits::deprecated_snippet::DeprecatedSnippet;
-use crate::InitVmState;
+use crate::traits::basic_snippet::Reviewer;
+use crate::traits::basic_snippet::SignOffFingerprint;
 
-#[derive(Clone, Debug)]
+/// Integer-divide the argument by 2.
+///
+/// ### Behavior
+///
+/// ```text
+/// BEFORE: _ [arg: u64]
+/// AFTER:  _ [arg/2: u64]
+/// ```
+///
+/// ### Preconditions
+///
+/// - the input is properly [`BFieldCodec`] encoded
+///
+/// ### Postconditions
+///
+/// - the output is properly [`BFieldCodec`] encoded
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 pub struct Div2;
 
-impl DeprecatedSnippet for Div2 {
-    fn entrypoint_name(&self) -> String {
+impl BasicSnippet for Div2 {
+    fn inputs(&self) -> Vec<(DataType, String)> {
+        vec![(DataType::U64, "arg".to_string())]
+    }
+
+    fn outputs(&self) -> Vec<(DataType, String)> {
+        vec![(DataType::U64, "(arg/2)".to_string())]
+    }
+
+    fn entrypoint(&self) -> String {
         "tasmlib_arithmetic_u64_div2".to_string()
     }
 
-    fn input_field_names(&self) -> Vec<String> {
-        vec!["value_hi".to_string(), "value_lo".to_string()]
-    }
+    fn code(&self, _: &mut Library) -> Vec<LabelledInstruction> {
+        triton_asm!(
+            // BEFORE: _ arg_hi arg_lo
+            // AFTER:  _ (arg / 2)_hi (arg / 2)_lo
+            {self.entrypoint()}:
+                /* divide low part */
+                push 2
+                pick 1
+                div_mod
+                pop 1
+                // _ arg_hi (arg_lo / 2)
 
-    fn input_types(&self) -> Vec<DataType> {
-        vec![DataType::U64]
-    }
+                /* divide high part, carry its least significant bit into the low part */
+                push 2
+                pick 2
+                div_mod
+                // _ (arg_lo / 2) (arg_hi / 2) (arg_hi % 2)
+                // _ (arg_lo / 2) (arg / 2)_hi (arg_hi % 2)
 
-    fn output_field_names(&self) -> Vec<String> {
-        vec!["(value / 2)_hi".to_string(), "(value / 2)_lo".to_string()]
-    }
+                push {1_u32 << 31}
+                hint two_pow_31: u32 = stack[0]
+                mul
+                hint carry: u32 = stack[0]
+                // _ (arg_lo / 2) (arg / 2)_hi carry
 
-    fn output_types(&self) -> Vec<DataType> {
-        vec![DataType::U64]
-    }
+                pick 2
+                add
+                // _ (arg / 2)_hi (arg / 2)_lo
 
-    fn stack_diff(&self) -> isize {
-        0
-    }
-
-    fn function_code(&self, _library: &mut Library) -> String {
-        let entrypoint = self.entrypoint_name();
-        let two_pow_31 = 1u64 << 31;
-
-        format!(
-            "
-                // BEFORE: _ value_hi value_lo
-                // AFTER:  _ (value / 2)_hi (value / 2)_lo
-                {entrypoint}:
-                    // Divide the lower number
-                    push 2
-                    swap 1
-                    div_mod
-                    pop 1
-                    // stack: _ value_hi (value_lo / 2)
-
-                    // Divide the upper number and carry its least significant bit into the lower number
-                    swap 1
-                    // stack: _ (value_lo / 2) value_hi
-
-                    push 2
-                    swap 1
-                    div_mod
-                    // stack: _ (value_lo / 2) (value_hi / 2) (value_hi % 2)
-
-                    push {two_pow_31}
-                    mul
-                    // stack: _ (value_lo / 2) (value_hi / 2) carry
-
-                    swap 1
-                    swap 2
-                    // stack: _ (value_hi / 2) carry (value_lo / 2)
-
-                    add
-                    // stack: _ (value / 2)_hi (value / 2)_lo
-
-                    return
-                "
+                return
         )
     }
 
-    fn crash_conditions(&self) -> Vec<String> {
-        vec![
-            "If value_hi is not a u32".to_string(),
-            "If value_lo is not a u32".to_string(),
-        ]
-    }
-
-    fn gen_input_states(&self) -> Vec<InitVmState> {
-        let n: u64 = rand::thread_rng().next_u64();
-        let n: U32s<2> = n.try_into().unwrap();
-        let mut input_stack = empty_stack();
-
-        push_encodable(&mut input_stack, &n);
-
-        vec![InitVmState::with_stack(input_stack)]
-    }
-
-    fn common_case_input_state(&self) -> InitVmState {
-        InitVmState::with_stack(
-            [
-                empty_stack(),
-                vec![BFieldElement::zero(), BFieldElement::new(1 << 31)],
-            ]
-            .concat(),
-        )
-    }
-
-    fn worst_case_input_state(&self) -> InitVmState {
-        let big_number = 1 << 31;
-        let worst_case_input = [big_number + 1, big_number].map(BFieldElement::new);
-        let worst_case_stack = [empty_stack(), worst_case_input.to_vec()].concat();
-
-        InitVmState::with_stack(worst_case_stack)
-    }
-
-    fn rust_shadowing(
-        &self,
-        stack: &mut Vec<BFieldElement>,
-        _std_in: Vec<BFieldElement>,
-        _secret_in: Vec<BFieldElement>,
-        _memory: &mut HashMap<BFieldElement, BFieldElement>,
-    ) {
-        let value_lo: u32 = stack.pop().unwrap().try_into().unwrap();
-        let value_hi: u32 = stack.pop().unwrap().try_into().unwrap();
-        let value: u64 = ((value_hi as u64) << 32) + value_lo as u64;
-        let result: u64 = value / 2;
-
-        stack.push(BFieldElement::new(result >> 32));
-        stack.push(BFieldElement::new(result & u32::MAX as u64));
+    fn sign_offs(&self) -> HashMap<Reviewer, SignOffFingerprint> {
+        let mut sign_offs = HashMap::new();
+        sign_offs.insert(Reviewer("ferdinand"), 0xe77a12ba30ef339b.into());
+        sign_offs
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use BFieldElement;
-
     use super::*;
-    use crate::empty_stack;
-    use crate::test_helpers::test_rust_equivalence_given_input_values_deprecated;
-    use crate::test_helpers::test_rust_equivalence_multiple_deprecated;
+    use crate::test_helpers::negative_test;
+    use crate::test_prelude::*;
 
-    #[test]
-    fn div2_u64_test() {
-        test_rust_equivalence_multiple_deprecated(&Div2, true);
+    impl Div2 {
+        fn assert_expected_behavior(&self, arg: u64) {
+            let initial_stack = self.set_up_test_stack(arg);
+
+            let mut expected_stack = initial_stack.clone();
+            self.rust_shadow(&mut expected_stack);
+
+            test_rust_equivalence_given_complete_state(
+                &ShadowedClosure::new(Self),
+                &initial_stack,
+                &[],
+                &NonDeterminism::default(),
+                &None,
+                Some(&expected_stack),
+            );
+        }
     }
 
-    #[should_panic]
-    #[test]
-    fn lo_is_not_u32() {
-        let mut init_stack = empty_stack();
-        init_stack.push(BFieldElement::new(16));
-        init_stack.push(BFieldElement::new(u32::MAX as u64 + 1));
+    impl Closure for Div2 {
+        type Args = u64;
 
-        test_rust_equivalence_given_input_values_deprecated::<Div2>(
-            &Div2,
-            &init_stack,
-            &[],
-            HashMap::default(),
-            None,
+        fn rust_shadow(&self, stack: &mut Vec<BFieldElement>) {
+            let arg = pop_encodable::<Self::Args>(stack);
+            push_encodable(stack, &(arg / 2));
+        }
+
+        fn pseudorandom_args(
+            &self,
+            seed: [u8; 32],
+            bench_case: Option<BenchmarkCase>,
+        ) -> Self::Args {
+            match bench_case {
+                Some(BenchmarkCase::CommonCase) => 0x8000_0000,
+                Some(BenchmarkCase::WorstCase) => 0xf000_0001_f000_0000,
+                None => StdRng::from_seed(seed).gen(),
+            }
+        }
+    }
+
+    #[test]
+    fn rust_shadow() {
+        ShadowedClosure::new(Div2).test();
+    }
+
+    #[proptest]
+    fn lo_is_not_u32(hi: u32, #[strategy(1_u64 << 32..)] lo: u64) {
+        let stack = [Div2.init_stack_for_isolated_run(), bfe_vec![hi, lo]].concat();
+
+        let error = InstructionError::OpStackError(OpStackError::FailedU32Conversion(bfe!(lo)));
+        negative_test(
+            &ShadowedClosure::new(Div2),
+            InitVmState::with_stack(stack),
+            &[error],
         );
     }
 
-    #[should_panic]
-    #[test]
-    fn hi_is_not_u32() {
-        let mut init_stack = empty_stack();
-        init_stack.push(BFieldElement::new(u32::MAX as u64 + 1));
-        init_stack.push(BFieldElement::new(16));
+    #[proptest]
+    fn hi_is_not_u32(#[strategy(1_u64 << 32..)] hi: u64, lo: u32) {
+        let stack = [Div2.init_stack_for_isolated_run(), bfe_vec![hi, lo]].concat();
 
-        test_rust_equivalence_given_input_values_deprecated::<Div2>(
-            &Div2,
-            &init_stack,
-            &[],
-            HashMap::default(),
-            None,
+        let error = InstructionError::OpStackError(OpStackError::FailedU32Conversion(bfe!(hi)));
+        negative_test(
+            &ShadowedClosure::new(Div2),
+            InitVmState::with_stack(stack),
+            &[error],
         );
     }
 
     #[test]
     fn div_2_test() {
-        prop_div_2(0);
-        prop_div_2(1);
-        prop_div_2(2);
-        prop_div_2(3);
-        prop_div_2(4);
-        prop_div_2(5);
-        prop_div_2(6);
-        prop_div_2(7);
-        prop_div_2(8);
-        prop_div_2(1 << 32);
-        prop_div_2((1 << 32) + 1);
-        prop_div_2((1 << 32) + 2);
-        prop_div_2((1 << 32) + 3);
-        prop_div_2((1 << 32) + 4);
-        prop_div_2((1 << 63) + 4);
-        prop_div_2((1 << 63) + 4);
-        prop_div_2((1 << 63) + (1 << 31));
-        prop_div_2((1 << 63) + (1 << 33) + (1 << 32) + (1 << 31));
+        let small_args = 0..9;
+        let mid_args = (0..9).map(|offset| (1 << 32) + offset);
+        let large_args = [0, 4, 1 << 31, 0b111 << 31].map(|offset| (1 << 63) + offset);
 
-        let mut rng = thread_rng();
-        for _ in 0..100 {
-            let value = rng.next_u64();
-            prop_div_2(value);
+        for arg in small_args.chain(mid_args).chain(large_args) {
+            Div2.assert_expected_behavior(arg);
         }
-    }
-
-    fn prop_div_2(value: u64) {
-        let mut init_stack = empty_stack();
-        init_stack.push(BFieldElement::new(value >> 32));
-        init_stack.push(BFieldElement::new(value & u32::MAX as u64));
-        let mut expected_stack = empty_stack();
-        let res = value / 2;
-        expected_stack.push(BFieldElement::new(res >> 32));
-        expected_stack.push(BFieldElement::new(res & u32::MAX as u64));
-
-        test_rust_equivalence_given_input_values_deprecated::<Div2>(
-            &Div2,
-            &init_stack,
-            &[],
-            HashMap::default(),
-            Some(&expected_stack),
-        );
     }
 }
 
 #[cfg(test)]
 mod benches {
     use super::*;
-    use crate::snippet_bencher::bench_and_write;
+    use crate::test_prelude::*;
 
     #[test]
-    fn div2_u64_benchmark() {
-        bench_and_write(Div2);
+    fn benchmark() {
+        ShadowedClosure::new(Div2).bench();
     }
 }
