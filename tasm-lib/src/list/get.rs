7f5729cacf049e278e01181a9959a24fe734931c
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2025-01-22 12:05:50 +0100

    refactor!: Check bounds in `list::get::Get`
    BREAKING CHANGE: Make fields of `list::get::Get` private. To construct a
    new instance, use function `new`. Also, remove the implementation of
    `DeprecatedSnippet` for `list::get::Get`, implement `BasicSnippet`
    directly.

--- tasm-lib/src/list/get.rs
+++ tasm-lib/src/list/get.rs
@@ -1,270 +1,161 @@
-use std::collections::HashMap;
-
-use itertools::Itertools;
-use rand::prelude::*;
 use triton_vm::prelude::*;
 
-use crate::empty_stack;
+use crate::list::length::Length;
 use crate::prelude::*;
-use crate::rust_shadowing_helper_functions::list::list_get;
-use crate::rust_shadowing_helper_functions::list::untyped_insert_random_list;
-use crate::traits::deprecated_snippet::DeprecatedSnippet;
-use crate::InitVmState;
 
+/// Read an element from a list. Performs bounds check.
+///
+/// Only supports lists with [statically sized](DataType::static_length)
+/// elements.
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct Get {
-    pub element_type: DataType,
+    element_type: DataType,
 }
 
 impl Get {
-    pub fn new(data_type: DataType) -> Self {
-        Self {
-            element_type: data_type,
-        }
+    pub const INDEX_OUT_OF_BOUNDS_ERROR_ID: i128 = 380;
+
+    /// Any part of the list is outside the allocated memory page.
+    /// See the [memory convention][crate::memory] for more details.
+    pub const MEM_PAGE_ACCESS_VIOLATION_ERROR_ID: i128 = 381;
+
+    /// # Panics
+    ///
+    /// Panics if the element has [dynamic length][DataType::static_length].
+    pub fn new(element_type: DataType) -> Self {
+        let has_static_len = element_type.static_length().is_some();
+        assert!(has_static_len, "element should have static length");
+
+        Self { element_type }
     }
 }
 
-impl DeprecatedSnippet for Get {
-    fn entrypoint_name(&self) -> String {
-        format!(
-            "tasmlib_list_get_element___{}",
-            self.element_type.label_friendly_name()
-        )
-    }
+impl BasicSnippet for Get {
+    fn inputs(&self) -> Vec<(DataType, String)> {
+        let list_type = DataType::List(Box::new(self.element_type.clone()));
 
-    fn input_field_names(&self) -> Vec<String> {
-        vec!["*list".to_string(), "index".to_string()]
-    }
-
-    fn input_types(&self) -> Vec<DataType> {
         vec![
-            DataType::List(Box::new(self.element_type.clone())),
-            DataType::U32,
+            (list_type, "*list".to_string()),
+            (DataType::U32, "index".to_string()),
         ]
     }
 
-    fn output_field_names(&self) -> Vec<String> {
-        // This function returns element_0 on the top of the stack and the other elements below it.
-        // E.g.: _ elem_2 elem_1 elem_0
-        let mut ret: Vec<String> = vec![];
-        let size = self.element_type.stack_size();
-        for i in 0..size {
-            ret.push(format!("element_{}", size - 1 - i));
-        }
-
-        ret
+    fn outputs(&self) -> Vec<(DataType, String)> {
+        vec![(self.element_type.clone(), "element".to_string())]
     }
 
-    fn output_types(&self) -> Vec<DataType> {
-        vec![DataType::Bfe; self.element_type.stack_size()]
+    fn entrypoint(&self) -> String {
+        let element_type = self.element_type.label_friendly_name();
+        format!("tasmlib_list_get_element___{element_type}")
     }
 
-    fn stack_diff(&self) -> isize {
-        self.element_type.stack_size() as isize - 2
-    }
-
-    fn function_code(&self, _library: &mut Library) -> String {
-        let entrypoint = self.entrypoint_name();
-        // Code to read an element from a list. No bounds-check.
-
-        let element_size = self.element_type.stack_size();
-
-        let mul_with_size = match element_size {
-            1 => vec![],
-            _ => triton_asm!(push {element_size} mul),
+    fn code(&self, library: &mut Library) -> Vec<LabelledInstruction> {
+        let mul_with_element_size = match self.element_type.stack_size() {
+            1 => triton_asm!(/* no-op */),
+            n => triton_asm!(push {n} mul),
         };
+
+        let list_length = library.import(Box::new(Length));
 
         triton_asm!(
             // BEFORE: _ *list index
-            // AFTER:  _ elem{N - 1}, elem{N - 2}, ..., elem{0}
-            {entrypoint}:
-                push 1
-                add
-                {&mul_with_size}
-                // stack: _ *list (N * (index + 1))
+            // AFTER:  _ [element: self.element_type]
+            {self.entrypoint()}:
+                /* assert access is in bounds */
+                dup 1
+                call {list_length}  // _ *list index len
+                dup 1
+                lt                  // _ *list index (index < len)
+                assert error_id {Self::INDEX_OUT_OF_BOUNDS_ERROR_ID}
 
-                // assert read access is in bounds
-                split swap 1 push 0 eq assert
-                // stack: _ *list (N * (index + 1))
+                addi 1
+                {&mul_with_element_size}
+                                    // _ *list last_word_offset
 
-                add
-                // stack: _ (*list + N * index + 1)
+                /* assert access is within one memory page */
+                split
+                pick 1
+                push 0
+                eq
+                assert error_id {Self::MEM_PAGE_ACCESS_VIOLATION_ERROR_ID}
+                                    // _ *list last_word_offset
 
+                add                 // _ *element_last_word
                 {&self.element_type.read_value_from_memory_pop_pointer()}
-                // stack: _ elem{{N - 1}}, elem{{N - 2}}, ..., elem{{0}}
-
+                                    // _ element
                 return
         )
-        .iter()
-        .join("\n")
-    }
-
-    fn crash_conditions(&self) -> Vec<String> {
-        vec![]
-    }
-
-    fn gen_input_states(&self) -> Vec<InitVmState> {
-        let mut rng = thread_rng();
-        let list_length = rng.gen_range(1..=100);
-        let index_to_read = rng.gen_range(0..list_length);
-        vec![self.input_state(list_length, index_to_read)]
-    }
-
-    fn common_case_input_state(&self) -> InitVmState {
-        self.input_state(1 << 5, 1 << 4)
-    }
-
-    fn worst_case_input_state(&self) -> InitVmState {
-        self.input_state(1 << 6, (1 << 6) - 1)
-    }
-
-    fn rust_shadowing(
-        &self,
-        stack: &mut Vec<BFieldElement>,
-        _std_in: Vec<BFieldElement>,
-        _secret_in: Vec<BFieldElement>,
-        memory: &mut HashMap<BFieldElement, BFieldElement>,
-    ) {
-        let index: u32 = stack.pop().unwrap().try_into().unwrap();
-        let list_pointer = stack.pop().unwrap();
-        let element: Vec<BFieldElement> = list_get(
-            list_pointer,
-            index as usize,
-            memory,
-            self.element_type.stack_size(),
-        );
-
-        stack.extend(element.into_iter().rev());
-    }
-}
-
-impl Get {
-    fn input_state(&self, list_length: usize, index: usize) -> InitVmState {
-        let list_pointer: u32 = random();
-        let list_pointer = BFieldElement::new(list_pointer as u64);
-        let mut stack = empty_stack();
-        stack.push(list_pointer);
-        stack.push(BFieldElement::new(index as u64));
-
-        let mut memory = HashMap::default();
-
-        untyped_insert_random_list(
-            list_pointer,
-            list_length,
-            &mut memory,
-            self.element_type.stack_size(),
-        );
-
-        InitVmState::with_stack_and_memory(stack, memory)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::empty_stack;
-    use crate::test_helpers::test_rust_equivalence_given_input_values_deprecated;
-    use crate::test_helpers::test_rust_equivalence_multiple_deprecated;
+    use crate::rust_shadowing_helper_functions::list::insert_random_list;
+    use crate::rust_shadowing_helper_functions::list::list_get;
+    use crate::test_prelude::*;
+    use crate::U32_TO_USIZE_ERR;
 
-    #[test]
-    fn new_snippet_test() {
-        test_rust_equivalence_multiple_deprecated(
-            &Get {
-                element_type: DataType::Xfe,
-            },
-            true,
-        );
-    }
+    impl Accessor for Get {
+        fn rust_shadow(
+            &self,
+            stack: &mut Vec<BFieldElement>,
+            memory: &HashMap<BFieldElement, BFieldElement>,
+        ) {
+            let index: u32 = stack.pop().unwrap().try_into().unwrap();
+            let list_pointer = stack.pop().unwrap();
 
-    #[test]
-    fn get_simple_1() {
-        let list_address = BFieldElement::new(48);
-        let list_length = 10;
-        for i in 0..list_length {
-            prop_get(DataType::Bfe, list_address, i, list_length);
+            let index: usize = index.try_into().expect(U32_TO_USIZE_ERR);
+            let element_length = self.element_type.static_length().unwrap();
+            let element = list_get(list_pointer, index, memory, element_length);
+
+            stack.extend(element.into_iter().rev());
+        }
+
+        fn pseudorandom_initial_state(
+            &self,
+            seed: [u8; 32],
+            bench_case: Option<BenchmarkCase>,
+        ) -> AccessorInitialState {
+            let mut rng = StdRng::from_seed(seed);
+            let list_length = match bench_case {
+                Some(BenchmarkCase::CommonCase) => 1 << 5,
+                Some(BenchmarkCase::WorstCase) => 1 << 6,
+                None => rng.gen_range(1..=100),
+            };
+            let index = match bench_case {
+                Some(BenchmarkCase::CommonCase) => list_length / 2,
+                Some(BenchmarkCase::WorstCase) => list_length - 1,
+                None => rng.gen_range(0..list_length),
+            };
+
+            let mut memory = HashMap::default();
+            let list_pointer = rng.gen();
+            insert_random_list(&self.element_type, list_pointer, list_length, &mut memory);
+
+            let mut stack = self.init_stack_for_isolated_run();
+            stack.push(list_pointer);
+            stack.push(bfe!(index));
+
+            AccessorInitialState { stack, memory }
         }
     }
 
     #[test]
-    fn get_simple_2() {
-        let list_address = BFieldElement::new(48);
-        let list_length = 10;
-        for i in 0..list_length {
-            prop_get(DataType::U64, list_address, i, list_length);
+    fn rust_shadow() {
+        for ty in [DataType::Bfe, DataType::Digest, DataType::I128] {
+            ShadowedAccessor::new(Get::new(ty)).test();
         }
-    }
-
-    #[test]
-    fn get_simple_3() {
-        let list_address = BFieldElement::new(48);
-        let list_length = 10;
-        for i in 0..list_length {
-            prop_get(DataType::Xfe, list_address, i, list_length);
-        }
-    }
-
-    #[test]
-    fn get_simple_15() {
-        let list_address = BFieldElement::new(48);
-        let list_length = 10;
-        for i in 0..list_length {
-            prop_get(
-                DataType::Tuple(vec![DataType::Digest; 3]),
-                list_address,
-                i,
-                list_length,
-            );
-        }
-    }
-
-    fn prop_get(data_type: DataType, list_pointer: BFieldElement, index: u32, list_length: u32) {
-        let mut init_stack = empty_stack();
-        init_stack.push(list_pointer);
-        init_stack.push(BFieldElement::new(index as u64));
-
-        let mut memory = HashMap::default();
-
-        // Insert length indicator of list, lives on offset = 0 from `list_address`
-        memory.insert(list_pointer, BFieldElement::new(list_length as u64));
-
-        // Insert random values for the elements in the list
-        let element_size = data_type.stack_size();
-        let mut rng = thread_rng();
-        let mut j = 1;
-        for _ in 0..list_length {
-            let element = (0..element_size).map(|_| rng.gen()).collect_vec();
-            for elem in element.iter() {
-                memory.insert(list_pointer + BFieldElement::new(j), *elem);
-                j += 1;
-            }
-        }
-        let targeted_element: Vec<BFieldElement> =
-            list_get(list_pointer, index as usize, &memory, element_size);
-
-        let mut expected_end_stack = empty_stack();
-        for i in 0..element_size {
-            expected_end_stack.push(targeted_element[element_size - 1 - i]);
-        }
-
-        test_rust_equivalence_given_input_values_deprecated(
-            &Get {
-                element_type: data_type,
-            },
-            &init_stack,
-            &[],
-            memory,
-            Some(&expected_end_stack),
-        );
     }
 }
 
 #[cfg(test)]
 mod benches {
     use super::*;
-    use crate::snippet_bencher::bench_and_write;
+    use crate::test_prelude::*;
 
     #[test]
     fn get_benchmark() {
-        bench_and_write(Get::new(DataType::Digest));
+        ShadowedAccessor::new(Get::new(DataType::Digest)).bench();
     }
 }
