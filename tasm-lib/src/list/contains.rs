a399b06748c16620e3fa8901d9acb39dc80527a5
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2025-01-24 13:51:28 +0100

    refactor!: Make field of `Contains` private
    To construct a new instance, use function `Contains::new`.

--- tasm-lib/src/list/contains.rs
+++ tasm-lib/src/list/contains.rs
@@ -1,5 +1,6 @@
 use triton_vm::prelude::*;
 
+use crate::list::get::Get;
 use crate::prelude::*;
 
 /// Returns `true` if the list contains an element with the given value.
@@ -7,21 +8,53 @@ use crate::prelude::*;
 /// This operation is *O*(*n*).
 ///
 /// Mirrors the `contains` method from Rust `core` as closely as possible.
+///
+/// Only supports lists with [statically sized](BFieldCodec::static_length)
+/// elements. The element's static size must be in range `1..=14`.
+///
+/// ### Behavior
+///
+/// ```text
+/// BEFORE: _ *list [needle: ElementType]
+/// AFTER:  _ [needle ∈ list: bool]
+/// ```
+///
+/// ### Preconditions
+///
+/// - the argument `*list` points to a properly [`BFieldCodec`]-encoded list
+/// - all input arguments are properly [`BFieldCodec`] encoded
+///
+/// ### Postconditions
+///
+/// - the output is properly [`BFieldCodec`] encoded
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct Contains {
-    pub element_type: DataType,
+    element_type: DataType,
+}
+
+impl Contains {
+    /// # Panics
+    ///
+    /// Panics
+    /// - if the element has [dynamic length][BFieldCodec::static_length], or
+    /// - if the static length is 0, or
+    /// - if the static length is larger than or equal to 15.
+    // Requirement “static length < 15” is needed for comparing elements.
+    pub fn new(element_type: DataType) -> Self {
+        Get::assert_element_type_is_supported(&element_type);
+
+        Self { element_type }
+    }
 }
 
 impl BasicSnippet for Contains {
     fn inputs(&self) -> Vec<(DataType, String)> {
+        let element_type = self.element_type.clone();
+        let list_type = DataType::List(Box::new(element_type.clone()));
+
         vec![
-            (
-                DataType::List(Box::new(self.element_type.clone())),
-                "self".to_owned(),
-            ),
-            // `x` denotes the value you're looking for, the "needle". This
-            // name is taken from the rust-implementation of `contains`.
-            (self.element_type.clone(), "x".to_owned()),
+            (list_type, "self".to_owned()),
+            (element_type, "needle".to_owned()),
         ]
     }
 
@@ -30,120 +63,79 @@ impl BasicSnippet for Contains {
     }
 
     fn entrypoint(&self) -> String {
-        format!(
-            "tasmlib_list_contains___{}",
-            self.element_type.label_friendly_name()
-        )
+        let element_type = self.element_type.label_friendly_name();
+        format!("tasmlib_list_contains___{element_type}")
     }
 
     fn code(&self, library: &mut Library) -> Vec<LabelledInstruction> {
+        // unwrap is fine: Self::new checks range of stack size
+        let element_size = self.element_type.stack_size().try_into().unwrap();
+        let needle_alloc = library.kmalloc(element_size);
+
         let entrypoint = self.entrypoint();
-        let elem_size: u32 = self.element_type.stack_size().try_into().unwrap();
-        let elem_size_plus_one = elem_size + 1;
-
-        let needle_alloc = library.kmalloc(elem_size);
-
         let loop_label = format!("{entrypoint}_loop");
-
-        let read_element = self.element_type.read_value_from_memory_leave_pointer();
-        let eq_code = self.element_type.compare();
-
-        let loop_code = triton_asm!(
-            // INVARIANT: _ match_found *list *list[i]
-            /* loop's end-condition: all elements checked, or a match is found */
-            {loop_label}:
-                /* Loop header */
-                dup 1 dup 1 eq
-                // _ match_found *list *list[i] (*list == *list[i])
-
-                skiz return
-                // _ match_found *list *list[i]
-
-                dup 2
-                // _ match_found *list *list[i] match_found
-
-                skiz return
-                // _ 0 *list *list[i]
-
-
-                /* Loop body */
-                dup 0
-                {&read_element}
-                // _ 0 *list *list[i] [haystack] *list[i-1]
-
-                swap {elem_size_plus_one}
-                pop 1
-                // _ 0 *list *list[i-1] [haystack]
-
-                push {needle_alloc.read_address()}
-                {&read_element}
-                pop 1
-                // _ 0 *list *list[i-1] [haystack] [needle]
-
-                {&eq_code}
-                // _ 0 *list *list[i-1] (haystack == needle)
-
-                swap 3
-                pop 1
-                // _ (haystack == needle) *list *list[i-1]
-
-
-                recurse
-        );
-
-        let write_element = self.element_type.write_value_to_memory_leave_pointer();
-        let mul_with_elem_size = match elem_size {
-            1 => triton_asm!(),
+        let mul_with_element_size = match element_size {
+            1 => triton_asm!(), // no-op
             n => triton_asm!(push {n} mul),
         };
 
         triton_asm!(
             // BEFORE: _ *list [value]
-            // AFTER: match_found
+            // AFTER:  _ match_found
             {entrypoint}:
                 push {needle_alloc.write_address()}
-                {&write_element}
-                pop 1
-                // _ *list
+                {&self.element_type.write_value_to_memory_leave_pointer()}
+                pop 1           // _ *list
 
-                push 0
-                swap 1
-                // _ 0 *list
+                push 0          hint match_found: bool = stack[0]
+                pick 1          // _ 0 *list
 
                 dup 0
-                read_mem 1
-                // _ 0 *list list_len (*list - 1)
-
-                push 1 add
-                // _ 0 *list list_len *list
-
-                swap 1
-                // _ 0 *list *list list_len
-
-                {&mul_with_elem_size}
-                // _ 0 *list *list (list_len * elem_size)
-
-                add
-                // _ 0 *list *list_last_word
+                read_mem 1      // _ 0 *list list_len (*list - 1)
+                addi 1          // _ 0 *list list_len *list
+                pick 1          // _ 0 *list *list list_len
+                {&mul_with_element_size}
+                                // _ 0 *list *list (list_len * elem_size)
+                add             // _ 0 *list *list_last_word
 
                 call {loop_label}
-                // _ match_found *list *list_last_word
-
-                pop 2
-                // _ match_found
+                                // _ match_found *list *list_last_word
+                pop 2           // _ match_found
 
                 return
 
-                {&loop_code}
+            // INVARIANT: _ match_found *list *list[i]
+            {loop_label}:
+                /* loop header – all elements checked, or match found? */
+                dup 1
+                dup 1
+                eq              // _ match_found *list *list[i] (*list == *list[i])
+                dup 3
+                add             // _ match_found *list *list[i] ((*list == *list[i]) || match_found)
+                skiz return     // _ 0           *list *list[i]
+
+
+                /* Loop body */
+                {&self.element_type.read_value_from_memory_leave_pointer()}
+                                // _ 0 *list [haystack_element] *list[i-1]
+                place {self.element_type.stack_size()}
+                                // _ 0 *list *list[i-1] [haystack_element]
+
+                push {needle_alloc.read_address()}
+                {&self.element_type.read_value_from_memory_pop_pointer()}
+                                // _ 0 *list *list[i-1] [haystack_element] [needle]
+                {&self.element_type.compare()}
+                                // _ 0 *list *list[i-1] (haystack_element == needle)
+
+                swap 3
+                pop 1           // _ (haystack_element == needle) *list *list[i-1]
+                recurse
         )
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use num::One;
-    use num::Zero;
-
     use super::*;
     use crate::library::STATIC_MEMORY_FIRST_ADDRESS;
     use crate::rust_shadowing_helper_functions::list::load_list_unstructured;
@@ -152,8 +144,7 @@ mod tests {
 
     impl Contains {
         fn static_pointer_isolated_run(&self) -> BFieldElement {
-            STATIC_MEMORY_FIRST_ADDRESS - bfe!(self.element_type.stack_size() as u64)
-                + BFieldElement::one()
+            STATIC_MEMORY_FIRST_ADDRESS - bfe!(self.element_type.stack_size()) + bfe!(1)
         }
 
         fn prepare_state(
@@ -164,7 +155,7 @@ mod tests {
         ) -> FunctionInitialState {
             let mut memory: HashMap<BFieldElement, BFieldElement> = HashMap::default();
             let list_length = haystack_elements.len();
-            memory.insert(list_pointer, bfe!(list_length as u64));
+            memory.insert(list_pointer, bfe!(list_length));
             let mut word_pointer = list_pointer;
             word_pointer.increment();
             for rand_elem in haystack_elements.iter() {
@@ -188,89 +179,15 @@ mod tests {
         }
     }
 
-    #[test]
-    fn contains_returns_true_on_contained_value() {
-        let snippet = Contains {
-            element_type: DataType::U64,
-        };
-        let a_u64_element = vec![bfe!(2), bfe!(3)];
-        let u64_list = vec![a_u64_element.clone()];
-        let init_state = snippet.prepare_state(BFieldElement::zero(), a_u64_element, u64_list);
-        let nd = NonDeterminism::default().with_ram(init_state.memory);
-
-        let expected_final_stack = [
-            snippet.init_stack_for_isolated_run(),
-            vec![BFieldElement::one()],
-        ]
-        .concat();
-
-        test_rust_equivalence_given_complete_state(
-            &ShadowedFunction::new(snippet),
-            &init_state.stack,
-            &[],
-            &nd,
-            &None,
-            Some(&expected_final_stack),
-        );
-    }
-
-    #[test]
-    fn contains_returns_false_on_mirrored_value() {
-        let snippet = Contains {
-            element_type: DataType::U64,
-        };
-        let a_u64_element = vec![bfe!(2), bfe!(3)];
-        let mirrored_u64_element = vec![bfe!(3), bfe!(2)];
-        let init_state = snippet.prepare_state(
-            BFieldElement::zero(),
-            a_u64_element,
-            vec![mirrored_u64_element],
-        );
-        let nd = NonDeterminism::default().with_ram(init_state.memory);
-
-        let expected_final_stack = [
-            snippet.init_stack_for_isolated_run(),
-            vec![BFieldElement::zero()],
-        ]
-        .concat();
-
-        test_rust_equivalence_given_complete_state(
-            &ShadowedFunction::new(Contains {
-                element_type: DataType::U64,
-            }),
-            &init_state.stack,
-            &[],
-            &nd,
-            &None,
-            Some(&expected_final_stack),
-        );
-    }
-
-    #[test]
-    fn contains_pbt() {
-        for element_type in [
-            DataType::Bfe,
-            DataType::U32,
-            DataType::U64,
-            DataType::Xfe,
-            DataType::U128,
-            DataType::Digest,
-            DataType::Tuple(vec![DataType::Digest, DataType::Digest]),
-        ] {
-            ShadowedFunction::new(Contains { element_type }).test()
-        }
-    }
-
     impl Function for Contains {
         fn rust_shadow(
             &self,
             stack: &mut Vec<BFieldElement>,
             memory: &mut HashMap<BFieldElement, BFieldElement>,
         ) {
-            let mut needle = vec![];
-            for _ in 0..self.element_type.stack_size() {
-                needle.push(stack.pop().unwrap());
-            }
+            let needle = (0..self.element_type.stack_size())
+                .map(|_| stack.pop().unwrap())
+                .collect_vec();
 
             let haystack_list_ptr = stack.pop().unwrap();
             let haystack_elems =
@@ -329,15 +246,12 @@ mod tests {
         }
 
         fn corner_case_initial_states(&self) -> Vec<FunctionInitialState> {
-            let empty_list = self.prepare_state(
-                BFieldElement::one(),
-                vec![BFieldElement::one(); self.element_type.stack_size()],
-                vec![],
-            );
+            let empty_list =
+                self.prepare_state(bfe!(1), bfe_vec![1; self.element_type.stack_size()], vec![]);
 
-            let an_element = vec![BFieldElement::new(42); self.element_type.stack_size()];
-            let another_element = vec![BFieldElement::new(420); self.element_type.stack_size()];
-            let a_pointer = BFieldElement::new(42);
+            let an_element = bfe_vec![42; self.element_type.stack_size()];
+            let another_element = bfe_vec![420; self.element_type.stack_size()];
+            let a_pointer = bfe!(42);
             let one_element_match =
                 self.prepare_state(a_pointer, an_element.clone(), vec![an_element.clone()]);
             let one_element_no_match =
@@ -364,7 +278,7 @@ mod tests {
             );
 
             let non_symmetric_value = (0..self.element_type.stack_size())
-                .map(|i| bfe!(i as u64 + 200))
+                .map(|i| bfe!(i + 200))
                 .collect_vec();
             let mut mirrored_non_symmetric_value = non_symmetric_value.clone();
             mirrored_non_symmetric_value.reverse();
@@ -386,6 +300,61 @@ mod tests {
             ]
         }
     }
+
+    #[test]
+    fn rust_shadow() {
+        for element_type in [
+            DataType::Bfe,
+            DataType::U32,
+            DataType::U64,
+            DataType::Xfe,
+            DataType::U128,
+            DataType::Digest,
+            DataType::Tuple(vec![DataType::Digest, DataType::Digest]),
+        ] {
+            ShadowedFunction::new(Contains::new(element_type)).test()
+        }
+    }
+
+    #[test]
+    fn contains_returns_true_on_contained_value() {
+        let snippet = Contains::new(DataType::U64);
+        let a_u64_element = bfe_vec![2, 3];
+        let u64_list = vec![a_u64_element.clone()];
+        let init_state = snippet.prepare_state(bfe!(0), a_u64_element, u64_list);
+        let nd = NonDeterminism::default().with_ram(init_state.memory);
+
+        let expected_final_stack = [snippet.init_stack_for_isolated_run(), bfe_vec![1]].concat();
+
+        test_rust_equivalence_given_complete_state(
+            &ShadowedFunction::new(snippet),
+            &init_state.stack,
+            &[],
+            &nd,
+            &None,
+            Some(&expected_final_stack),
+        );
+    }
+
+    #[test]
+    fn contains_returns_false_on_mirrored_value() {
+        let snippet = Contains::new(DataType::U64);
+        let a_u64_element = bfe_vec![2, 3];
+        let mirrored_u64_element = bfe_vec![3, 2];
+        let init_state = snippet.prepare_state(bfe!(0), a_u64_element, vec![mirrored_u64_element]);
+        let nd = NonDeterminism::default().with_ram(init_state.memory);
+
+        let expected_final_stack = [snippet.init_stack_for_isolated_run(), bfe_vec![0]].concat();
+
+        test_rust_equivalence_given_complete_state(
+            &ShadowedFunction::new(Contains::new(DataType::U64)),
+            &init_state.stack,
+            &[],
+            &nd,
+            &None,
+            Some(&expected_final_stack),
+        );
+    }
 }
 
 #[cfg(test)]
@@ -396,7 +365,7 @@ mod benches {
     #[test]
     fn benchmark() {
         for element_type in [DataType::U64, DataType::Digest] {
-            ShadowedFunction::new(Contains { element_type }).bench();
+            ShadowedFunction::new(Contains::new(element_type)).bench();
         }
     }
 }
